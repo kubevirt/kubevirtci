0df89f0eec163d81e10f11fd95bf0173a668a467
Roman Mohr <rmohr@redhat.com>
2018-02-14 16:36:25 +0100

    Run rmohr/cli container privileged
    We need to access the docker socket, so make the container privileged.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- kubeadm-1.9.3/run.sh
+++ kubeadm-1.9.3/run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock rmohr/cli:latest --nodes 2 --base rmohr/kubeadm-1.9.3
+docker run --privileged --rm -v /var/run/docker.sock:/var/run/docker.sock rmohr/cli:latest --nodes 2 --base rmohr/kubeadm-1.9.3
