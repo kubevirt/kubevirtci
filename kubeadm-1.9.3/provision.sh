dfb1da721e1965ed7187acee4420bb9f82b6ae91
Roman Mohr <rmohr@redhat.com>
2018-02-14 16:31:46 +0100

    Set a reasonable tag for the provisioned vm
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- kubeadm-1.9.3/provision.sh
+++ kubeadm-1.9.3/provision.sh
@@ -4,4 +4,4 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run --rm -v ${DIR}/scripts/:/scripts/ -v /var/run/docker.sock:/var/run/docker.sock rmohr/cli --provision --scripts /scripts --base rmohr/centos:1608_01 --tag test
+docker run --rm -v ${DIR}/scripts/:/scripts/ -v /var/run/docker.sock:/var/run/docker.sock rmohr/cli --provision --scripts /scripts --base rmohr/centos:1608_01 --tag rmohr/kubeadm-1.9.3
