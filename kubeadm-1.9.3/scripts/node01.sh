388a5a436d3b3ee88a27a6ffce7347358a543f3b
David Vossel <davidvossel@gmail.com>
2018-02-21 17:07:52 -0500

    Add admission registration feature to k8s cluster
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- kubeadm-1.9.3/scripts/node01.sh
+++ kubeadm-1.9.3/scripts/node01.sh
@@ -2,6 +2,15 @@
 
 set -e
 
-kubeadm init --pod-network-cidr=10.244.0.0/16 --kubernetes-version v1.9.3 --token abcdef.1234567890123456
+kubeadm init --config <(cat <<EOF)
+  apiVersion: kubeadm.k8s.io/v1alpha1
+  kind: MasterConfiguration
+  apiServerExtraArgs:
+    runtime-config: admissionregistration.k8s.io/v1alpha1
+  token: abcdef.1234567890123456
+  pod-network-cidr: 10.244.0.0/16
+  kubernetes-version: v1.9.3
+EOF
+
 kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
 kubectl --kubeconfig=/etc/kubernetes/admin.conf taint nodes node01 node-role.kubernetes.io/master:NoSchedule-
