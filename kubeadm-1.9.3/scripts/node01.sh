d41670113fb624fd325dd6365ac4af01b1ad0dd2
Roman Mohr <rmohr@redhat.com>
2018-02-22 08:44:22 +0100

    Fix file based kubeadm configuration
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- kubeadm-1.9.3/scripts/node01.sh
+++ kubeadm-1.9.3/scripts/node01.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-kubeadm init --config <(cat <<EOF)
-  apiVersion: kubeadm.k8s.io/v1alpha1
-  kind: MasterConfiguration
-  apiServerExtraArgs:
-    runtime-config: admissionregistration.k8s.io/v1alpha1
-  token: abcdef.1234567890123456
-  pod-network-cidr: 10.244.0.0/16
-  kubernetes-version: v1.9.3
+cat > /etc/kubernetes/kubeadm.conf <<EOF
+apiVersion: kubeadm.k8s.io/v1alpha1
+kind: MasterConfiguration
+apiServerExtraArgs:
+  runtime-config: admissionregistration.k8s.io/v1alpha1
+token: abcdef.1234567890123456
+pod-network-cidr: 10.244.0.0/16
+kubernetes-version: v1.9.3
 EOF
+
+kubeadm init --config /etc/kubernetes/kubeadm.conf
 
 kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
 kubectl --kubeconfig=/etc/kubernetes/admin.conf taint nodes node01 node-role.kubernetes.io/master:NoSchedule-
