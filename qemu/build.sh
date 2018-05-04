f0de04ae119acb70fa79d01ff555eab20a446821
Roman Mohr <rmohr@redhat.com>
2018-05-04 23:31:31 +0200

    build distroless qemu container

--- qemu/build.sh
+++ qemu/build.sh
@@ -2,8 +2,9 @@
 
 set -ex
 
-docker build . -t qemu-builder
+docker build -f "$1" . -t qemu-builder
+mkdir bin
 id=$(docker create qemu-builder --entrypoint /)
-docker cp $id:/usr/local/bin/qemu-io .
-docker cp $id:/usr/local/bin/qemu-system-x86_64 .
+docker cp $id:/usr/local/bin/qemu-img "$(dirname $2)"
+docker cp $id:/usr/local/bin/qemu-system-x86_64 "$(dirname $2)"
 docker rm $id
