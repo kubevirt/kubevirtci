a4b418cf8d5a4d83ab3bdd5a473f2017c1f3a4ab
root <root@sscheink.tlv.csb>
2018-07-25 13:15:16 +0300

    Added the ability to wait until the network is fully ready.

--- k8s-multus/scripts/node01.sh
+++ k8s-multus/scripts/node01.sh
@@ -2,6 +2,17 @@
 
 set -ex
 
+# Wait for the network to really came up
+while [[ `cat /proc/sys/net/ipv4/ip_forward` -eq 0 ]]
+do
+ sleep 2
+done
+
+while [[ ! -f /proc/sys/net/bridge/bridge-nf-call-iptables ]]
+do
+ sleep 2
+done
+
 kubeadm init --config /etc/kubernetes/kubeadm.conf
 
 cd multus-cni/examples
@@ -12,4 +23,4 @@ kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f flannel-conf.yml
 kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f macvlan-conf.yml
 
 kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/intel/multus-cni/dev/network-plumbing-working-group-crd-change/examples/multus-with-flannel.yml
-kubectl --kubeconfig=/etc/kubernetes/admin.conf taint nodes node01 node-role.kubernetes.io/master:NoSchedule-
+kubectl --kubeconfig=/etc/kubernetes/admin.conf taint nodes node01 node-role.kubernetes.io/master:NoSchedule- 
--- k8s-multus/scripts/nodes.sh
+++ k8s-multus/scripts/nodes.sh
@@ -2,4 +2,15 @@
 
 set -ex
 
+# Wait for the network to really came up
+while [[ `cat /proc/sys/net/ipv4/ip_forward` -eq 0 ]]
+do
+ sleep 2
+done
+
+while [[ ! -f /proc/sys/net/bridge/bridge-nf-call-iptables ]]
+do
+ sleep 2
+done
+
 kubeadm join --token abcdef.1234567890123456 192.168.66.101:6443 --ignore-preflight-errors=all --discovery-token-unsafe-skip-ca-verification=true
