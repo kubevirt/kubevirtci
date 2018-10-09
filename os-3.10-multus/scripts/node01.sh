9307fe7580307503534410bea9d4a4329fc2ddff
Sebastian Sch <sebassch@gmail.com>
2018-10-09 14:49:05 +0300

    Add deprecate for the k8s-multus-1.10.4 provider.

--- os-3.10-multus/scripts/node01.sh
+++ os-3.10-multus/scripts/node01.sh
@@ -88,5 +88,8 @@ set -e
 # openshift-sdn remove the content of the cni folder on restart.
 # Needs to recreate the multus cni config after the openshift-sdn is up.
 oc -n kube-system delete po `oc get po -n kube-system | grep kube-multus-ds | awk '{print $1}'`
+oc -n kube-system delete po `oc get po -n kube-system | grep kube-cni-plugins | awk '{print $1}'`
+oc -n kube-system delete po `oc get po -n kube-system | grep ovs-cni-plugin | awk '{print $1}'`
+oc -n kube-system delete po `oc get po -n kube-system | grep ovs-vsctl-amd64 | awk '{print $1}'`
 
 /usr/bin/oc create -f /tmp/local-volume.yaml
