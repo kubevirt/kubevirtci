a71517fa2194717b6b92346b8cdad4230eefad50
Artyom Lukianov <alukiano@redhat.com>
2019-06-19 09:17:27 +0300

    Remove deprecated providers (#100)
    - k8s-multus-1.10.4
    - k8s-multus-1.11.1
    - k8s-multus-1.12.2
    - k8s-1.9.3
    - k8s-1.10.3
    - k8s-1.10.4
    - os-3.9
    - os-3.9-crio
    - os-3.10
    - os-3.10-crio
    - os-3.10-multus
    -

--- cluster-provision/k8s-multus/1.10.4/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export version=1.10.4
-../publish.sh
--- cluster-provision/os-3.9/scripts/nodes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-# disable all master components, the machine should be a node
-systemctl stop origin-master-api
-systemctl disable origin-master-api
-systemctl stop origin-master-controllers
-systemctl disable origin-master-controllers
