0b15819b2f5b892a58d501e9bde59ec1b16ed140
Quique Llorente <ellorent@redhat.com>
2019-06-21 11:51:50 +0200

    Compose correctly the secondary-nics at multinode
    From stated here [1] running with multinode and
    secondary nics fail since it was composing incorrectly
    the quemu args line.
    
    This remove the infinite concatenation and use unique
    device names per node.
    
    [1] https://github.com/kubevirt/kubevirtci/pull/105#discussion_r296142403
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster-provision/okd/4.1.0/run.sh
+++ cluster-provision/okd/4.1.0/run.sh
@@ -3,7 +3,7 @@
 set -x
 
 okd_image_hash="sha256:8a89ea659ffcfc6402d7d6ee43418bf2194b27ea74c239699e8268e29639aaa4"
-gocli_image_hash="sha256:ac77baa6561557bb66d0f669b7aa4383430f2a7e5388627102f268f5558c3a8b"
+gocli_image_hash="sha256:caf1c3f63dc5f3137795084e492a48c802a7ef2e7f7fbe1d67e5cff684d376a3"
 
 gocli="docker run --privileged --net=host --rm -t -v /var/run/docker.sock:/var/run/docker.sock docker.io/kubevirtci/gocli@${gocli_image_hash}"
 
