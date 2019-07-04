50dc4bba538bfe013e7cbb637ba1b6161ac19030
Marc Sluiter <msluiter@redhat.com>
2019-07-04 10:57:51 +0200

    Updated shasums

--- cluster-up/cluster/os-3.11.0-crio/provider.sh
+++ cluster-up/cluster/os-3.11.0-crio/provider.sh
@@ -4,4 +4,4 @@ set -e
 
 source ${KUBEVIRTCI_PATH}/cluster/os-3.11.0/provider.sh
 
-image="os-3.11.0-crio@sha256:5eb4a00c89a9dcadccd3360a43973ac38897511260ce192598d6d8731c0c3e7a"
+image="os-3.11.0-crio@sha256:862be28595834f8fa686f4f77869ee0ca051ef612fc66b4728034ac786a1a67f"
--- cluster-up/cluster/os-3.11.0-multus/provider.sh
+++ cluster-up/cluster/os-3.11.0-multus/provider.sh
@@ -4,4 +4,4 @@ set -e
 
 source ${KUBEVIRTCI_PATH}/cluster/os-3.11.0/provider.sh
 
-image="os-3.11.0-multus@sha256:f8de6bda57209553e1a9e428dca1432270b7db0e5206376898b93aea64afd053"
+image="os-3.11.0-multus@sha256:ffd92718c888645e0a55f750a7c9091eefc17c850e09b1490ad0fc9425c88459"
