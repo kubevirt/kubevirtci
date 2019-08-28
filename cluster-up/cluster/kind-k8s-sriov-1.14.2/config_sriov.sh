3c593e46eee6655a1aa8504180bf1710b0d160d1
Federico Paolinelli <fpaoline@redhat.com>
2019-08-28 17:46:52 +0200

    Replace the sriov cni image used by the operator with the upstream one. In ci we want to be able to the test mac address feature.

--- cluster-up/cluster/kind-k8s-sriov-1.14.2/config_sriov.sh
+++ cluster-up/cluster/kind-k8s-sriov-1.14.2/config_sriov.sh
@@ -22,6 +22,11 @@ function deploy_sriov_operator {
   fi
 
   pushd $OPERATOR_PATH
+    # TODO: right now in CI we need to use upstream sriov cni in order to have this
+    # https://github.com/intel/sriov-cni/pull/88 available. This can be removed once the feature will
+    # be merged in openshift sriov operator. We need latest since that feature was not tagged yet
+    sed -i '/SRIOV_CNI_IMAGE/!b;n;c\              value: nfvpe\/sriov-cni' ./deploy/operator.yaml
+
     # on prow nodes the default shell is dash and some commands are not working
     make deploy-setup-k8s SHELL=/bin/bash OPERATOR_EXEC=kubectl
   popd
