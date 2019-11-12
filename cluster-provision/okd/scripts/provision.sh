75fd05681ab4073633f0195dded081ca14f2b154
Quique Llorente <ellorent@redhat.com>
2019-11-12 16:42:07 +0100

    Add raw pull secrets token file
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster-provision/okd/scripts/provision.sh
+++ cluster-provision/okd/scripts/provision.sh
@@ -2,8 +2,10 @@
 
 set -xe
 
+
+
 if [ ! -z $INSTALLER_RELEASE_IMAGE ]; then
-    until  export INSTALLER_COMMIT=$(oc adm release info $INSTALLER_RELEASE_IMAGE --commits | grep installer | awk '{print $3}' | head -n 1); do
+    until  export INSTALLER_COMMIT=$(oc adm release info $INSTALLER_RELEASE_IMAGE --commits --registry-config=/etc/installer/token | grep installer | awk '{print $3}' | head -n 1); do
         sleep 1
     done
 fi
@@ -141,7 +143,7 @@ if [[ $INSTALLER_TAG =~ ^.*4\.3$ ]]; then
 fi
 
 # inject pull secret into install config
-cat /etc/installer/token >> $CLUSTER_DIR/install-config.yaml
+echo pullSecret: "'$(cat  /etc/installer/token)'" >> $CLUSTER_DIR/install-config.yaml
 
 # inject vagrant ssh public key into install config
 ssh_pub_key="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
