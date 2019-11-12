56f69bb5867db7517f70a0787b32570a861e124a
Félix Enrique Llorente Pastora <ellorent@redhat.com>
2019-11-12 13:32:15 +0100

    Update okd-4.3 hashes with scale-down check (#179)
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster-provision/okd/4.3/run.sh
+++ cluster-provision/okd/4.3/run.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-okd_image_hash="sha256:25cb0ec3161dd4444f68896fc7ea82e34208c4e511958c3824211641abdd4f50"
+okd_image_hash="sha256:63abc3884002a615712dfac5f42785be864ea62006892bf8a086ccdbca8b3d38"
 gocli_image_hash="sha256:5ab9913a535227766f814b3a497d0eb1eede1a86a289d3e8bfb6bbd91836f11c"
 
 gocli="docker run --privileged --net=host --rm -t -v /var/run/docker.sock:/var/run/docker.sock docker.io/kubevirtci/gocli@${gocli_image_hash}"
