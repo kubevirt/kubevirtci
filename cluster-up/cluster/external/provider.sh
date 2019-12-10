676e4d7288cad00231c5614062bff62966019711
Daniel Hiller <daniel.hiller.1972@gmail.com>
2019-12-10 09:16:45 +0100

    Address review comments
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- cluster-up/cluster/external/provider.sh
+++ cluster-up/cluster/external/provider.sh
@@ -8,9 +8,9 @@ function prepare_config() {
     BASE_PATH=${KUBEVIRTCI_CONFIG_PATH:-$PWD}
 
     # required for running tests within openshift ci, otherwise tests don't find the configuration
-    ln -f -s ${KUBECONFIG:-$HOME/.kube/config} ${BASE_PATH}/$KUBEVIRT_PROVIDER/.kubeconfig
+    ln -f -s "${KUBECONFIG:-$HOME/.kube/config}" "${BASE_PATH}/$KUBEVIRT_PROVIDER/.kubeconfig"
 
-    cat >${BASE_PATH}/$KUBEVIRT_PROVIDER/config-provider-$KUBEVIRT_PROVIDER.sh <<EOF
+    cat > "${BASE_PATH}/$KUBEVIRT_PROVIDER/config-provider-$KUBEVIRT_PROVIDER.sh" <<EOF
 kubeconfig=${BASE_PATH}/$KUBEVIRT_PROVIDER/.kubeconfig
 docker_tag=\${DOCKER_TAG}
 docker_prefix=\${DOCKER_PREFIX}
@@ -22,8 +22,7 @@ EOF
 # The external cluster is assumed to be up.  Do a simple check
 function up() {
     prepare_config
-    _kubectl version >/dev/null
-    if [ $? -ne 0 ]; then
+    if ! _kubectl version >/dev/null; then
         echo -e "\n*** Unable to reach external cluster.  Please check configuration ***"
         echo -e "*** Type \"kubectl config view\" for current settings               ***\n"
         exit 1
