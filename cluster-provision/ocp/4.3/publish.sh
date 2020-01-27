9b0bfa0b0d41c499d35d0a613dcc754548813bdd
Quique Llorente <ellorent@redhat.com>
2020-01-27 11:24:16 +0100

    ocp-4.3: Fix echos
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster-provision/ocp/4.3/publish.sh
+++ cluster-provision/ocp/4.3/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 tag=$(git log -1 --pretty=%h)-$(date +%s)
 destination="quay.io/kubevirtci/ocp-4.3:$tag"
-echo docker tag kubevirtci/ocp-4.3-provision:latest $destination
-echo docker push $destination
+docker tag kubevirtci/ocp-4.3-provision:latest $destination
+docker push $destination
