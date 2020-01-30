b1ddc56cde5514022abc28d8a3672f9822cc9a4f
Or Mergi <omergi@redhat.com>
2020-01-30 16:55:55 +0200

    CNI manifest file names and kubernetes versions map
    This map will corrolate between k8s version and the plugin we
    would like to deploy.
    
    Signed-off-by: Or Mergi <omergi@redhat.com>

---
+++ cluster-provision/k8s/scripts/cnis-map.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+declare -A CNI_MANIFESTS
+CNI_MANIFESTS[1.17.0]="calico.yaml"
+CNI_MANIFESTS[1.16.2]="flannel-ge-16.yaml"
+CNI_MANIFESTS[1.15.1]="flannel-ge-16.yaml"
+CNI_MANIFESTS[1.14.6]="flannel-ge-16.yaml"
+CNI_MANIFESTS[1.13.3]="flannel-ge-12.yaml"
+CNI_MANIFESTS[1.12.0]="flannel-ge-12.yaml"
+CNI_MANIFESTS[1.11.0]="flannel.yaml"
+CNI_MANIFESTS[1.10.11]="flannel.yaml"
+export CNI_MANIFESTS
