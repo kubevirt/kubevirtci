c444fab2c7b9d07f7f68a8890f2449663238bf4a
Or Mergi <omergi@redhat.com>
2020-02-04 14:46:04 +0200

    Migrate images.sh file to common-scripts directory.
    Change source path for this file at all files using it.
    
    Signed-off-by: Or Mergi <omergi@redhat.com>

--- cluster-provision/k8s-multus/provision.sh
+++ cluster-provision/k8s-multus/provision.sh
@@ -6,6 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
-source ../images.sh
+source ../common-scripts/images.sh
 
 ../cli/cli provision --prefix k8s-multus-${version}-provision --scripts ./scripts --k8s-version ${version} --base kubevirtci/${IMAGES[centos7]} --tag kubevirtci/k8s-multus-${version}
--- cluster-provision/k8s/provision.sh
+++ cluster-provision/k8s/provision.sh
@@ -7,6 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
-source ../images.sh
+source ../common-scripts/images.sh
 
 ../cli/cli provision --prefix k8s-${version}-provision --scripts ./scripts --k8s-version ${version} --base kubevirtci/${IMAGES[centos7]} --tag kubevirtci/k8s-${version}
