740c098476da8a092e692dde75d32afe8ee44540
Or Mergi <omergi@redhat.com>
2020-02-13 14:13:43 +0200

    Add config-cni.sh script to common-scripts
    This script configuring additional settings to cni plugin,
    and should run on provision stage.
    
    If in the future we will need to configure another setting to
    cni plugins we can simply add it to the correspondig function.
    
    Signed-off-by: Or Mergi  <omergi@redhat.com>

---
+++ cluster-provision/common-scripts/config-cni.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -e
+
+function configure_calico(){
+    cat <<EOF >/etc/NetworkManager/conf.d/calico.conf
+[keyfile]
+unmanaged-devices=interface-name:cali*;interface-name:tunl*
+EOF
+
+    sysctl -w net.netfilter.nf_conntrack_max=1000000
+    echo "net.netfilter.nf_conntrack_max=1000000" >> /etc/sysctl.conf
+
+    systemctl restart NetworkManager
+}
+
+function configure_cni(){
+    if [[ $1 =~ calico ]]; then
+        configure_calico
+    fi
+}
