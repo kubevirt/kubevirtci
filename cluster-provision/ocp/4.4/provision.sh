31c7428c7db63c51be7335999f14bb02a4a01b1c
Quique Llorente <ellorent@redhat.com>
2020-02-17 12:11:03 +0100

    Pin ocp-4.4 to latest nigtly
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster-provision/ocp/4.4/provision.sh
+++ cluster-provision/ocp/4.4/provision.sh
@@ -4,6 +4,8 @@ set -ex
 
 source $(dirname ${BASH_SOURCE[0]} )/../common.sh
 
+tag=$(curl https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp-dev-preview/latest-4.4/release.txt |grep Name| awk '{print $2}')
+
 # For ocp-4.4 we want OVNKubernetes
 ${gocli} provision okd \
 --prefix ocp-4.4-provision \
@@ -16,7 +18,7 @@ ${gocli} provision okd \
 --networking-type OVNKubernetes \
 --installer-pull-secret-file ${INSTALLER_PULL_SECRET} \
 --installer-repo-tag release-4.4 \
---installer-release-image registry.svc.ci.openshift.org/ocp/release:4.4 \
+--installer-release-image registry.svc.ci.openshift.org/ocp/release:$tag \
 "kubevirtci/okd-base@${okd_base_hash}"
 
 $KUBEVIRTCI_PATH/cluster-provision/ocp/check-cluster-up.sh 4.4
