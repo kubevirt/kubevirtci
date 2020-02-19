3b9db73e9a6c691d3fdff5b1efb8a2c7eb6ec424
Roman Mohr <rmohr@redhat.com>
2020-02-19 15:17:48 +0100

    Rename cluster-up folders according to the provision folders
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- cluster-provision/k8s/check-cluster-up.sh
+++ cluster-provision/k8s/check-cluster-up.sh
@@ -4,6 +4,8 @@
 set -exuo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+version="$(cat version | tr -d '\n')"
+provision_dir="$(basename $(pwd))"
 
 function cleanup {
   cd "$DIR" && cd ../..
@@ -14,11 +16,12 @@ function cleanup {
 (
   ksh="./cluster-up/kubectl.sh"
   cd "$DIR" && cd ../..
-  version="$(cat version | tr -d '\n')"
   export KUBEVIRTCI_PROVISION_CHECK=1
-  export KUBEVIRT_PROVIDER="k8s-$(basename $DIR)"
+  export KUBEVIRT_PROVIDER="k8s-${provision_dir}"
   export KUBEVIRT_NUM_NODES=2
   trap cleanup EXIT ERR SIGINT SIGTERM SIGQUIT
+  ls
+  exit 1
   bash -x ./cluster-up/up.sh
   ${ksh} wait --for=condition=Ready pod --all
   ${ksh} wait --for=condition=Ready pod -n kube-system --all
