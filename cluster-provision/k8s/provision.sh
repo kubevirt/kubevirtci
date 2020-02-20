987509aec05768c1b4a0f005033eecfa8165eaa8
Roman Mohr <rmohr@redhat.com>
2020-02-20 12:17:21 +0100

    Adjust tag names for pushed images
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- cluster-provision/k8s/provision.sh
+++ cluster-provision/k8s/provision.sh
@@ -16,4 +16,4 @@ cd $DIR
 source ../common-scripts/images.sh
 
 export SIMPLE_PROVISION=true
-../cli/cli provision --prefix k8s-${version}-provision --scripts ${provision_dir} --k8s-version ${version} --base kubevirtci/${IMAGES[centos7]} --tag kubevirtci/k8s-${version}
+../cli/cli provision --prefix k8s-${version}-provision --scripts ${provision_dir} --k8s-version ${version} --base kubevirtci/${IMAGES[centos7]} --tag kubevirtci/k8s-${provision_dir}
