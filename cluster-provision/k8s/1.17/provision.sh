95096c8189c8b620ddc1310e12388df2190e1cc8
Félix Enrique Llorente Pastora <ellorent@redhat.com>
2020-03-03 11:00:56 +0100

    k8s-1.17: Fix non interactive dnf install -y (#294)
    * k8s-1.17: Change kubernetes.repo url
    
    So repo_gpgchck is working.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>
    
    * Disable repo gpgcheck
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>
    
    * Update k8s-1.17 hash
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster-provision/k8s/1.17/provision.sh
+++ cluster-provision/k8s/1.17/provision.sh
@@ -65,10 +65,10 @@ systemctl restart docker
 cat <<EOF >/etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-x86_64
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
+repo_gpgcheck=0
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
