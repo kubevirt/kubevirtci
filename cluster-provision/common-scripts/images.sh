cccc4b9ceb8f2c8e51d1de9a95e03a5f2afbb009
Quique Llorente <ellorent@redhat.com>
2020-04-07 12:02:17 +0200

    Update base images hash
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster-provision/common-scripts/images.sh
+++ cluster-provision/common-scripts/images.sh
@@ -3,6 +3,6 @@
 set -e
 
 declare -A IMAGES
-IMAGES[fedora31]="fedora@sha256:6e32c9c0073bd79a435537a067f14e7f9b72e1ddd9229f711306a93b9252125a"
-IMAGES[centos7]="centos@sha256:6f2548dcc23489d0c945aef516781ae2ea678424c3760d1dafa0a83d29411713"
+IMAGES[fedora31]="fedora@sha256:df2fc0d1a0db48b821c61df3e5ec2ab5575599192454656943b85dbc0d8d582a"
+IMAGES[centos7]="centos@sha256:5b7a969911f2c6baf6ca8390f4da30c16dec3cacc7b5530649965c50da7240f1"
 export IMAGES
