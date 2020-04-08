4f00dfb872723bdcf21c73b524f5e9e10aa1db1d
Félix Enrique Llorente Pastora <ellorent@redhat.com>
2020-04-08 10:01:53 +0200

    Delete kubevirtci base (#338)
    * Delete kubevirtci base
    
    It's included as part of centos7,centos8 and fedora base images, this
    reduce the provision steps needed when when a change at base image is done,
    helping with automation effort and dev experience.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>
    
    * Update base images hash
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>
    
    * Use box.qcow2 size to create disk
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>
    
    * k8s-1.17: Don't grow root partition
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>
    
    * Update cluster-up shas
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster-provision/README.md
+++ cluster-provision/README.md
@@ -1,6 +1,5 @@
 # k8s clusters in QEMU in docker
 
-* `base` contains the base image with some scripts, qemu and dnsmasq
 * `centos7` adds a vagrant centos7 box to the image
 * `cli` contains a tool for provisioning, running and managing the containerized clusters
 * `k8s-1.10.11` k8s-1.10.11 cluster based on the centos7 image, provisioned with kubeadm
@@ -15,7 +14,6 @@
 
 * `kubevirtci/cli`: `sha256:1dd015dea4f12e6dcb8e31be3eeb677fed96f290ef4a4892a33c43d666053536`
 * `kubevirtci/gocli`: `sha256:220f55f6b1bcb3975d535948d335bd0e6b6297149a3eba1a4c14cad9ac80f80d`
-* `kubevirtci/base`: `sha256:850ac2e2828610b5f35f004f2a8a1ab23609a4c7891c8a1b68cbb7eef5f5dda0`
 * `kubevirtci/centos:1905_01`: `sha256:4b292b646f382d986c75a2be8ec49119a03467fe26dccc3a0886eb9e6e38c911`
 * `kubevirtci/centos:2001_01`: `sha256:6f2548dcc23489d0c945aef516781ae2ea678424c3760d1dafa0a83d29411713`
 * `kubevirtci/os-3.11.0-multus`: `sha256:0c8be10799490a1f86740eaa490063f51eab78b920540f0a2946abc5e0bf30fe`
---
+++ cluster-provision/centos8/scripts/dnsmasq.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+NUM_NODES=${NUM_NODES-1}
+NUM_SECONDARY_NICS=${NUM_SECONDARY_NICS-0}
+
+ip link add br0 type bridge
+ip link set dev br0 up
+ip addr add dev br0 192.168.66.02/24
+
+for i in $(seq 1 ${NUM_NODES}); do
+  n="$(printf "%02d" ${i})"
+  ip tuntap add dev tap${n} mode tap user $(whoami)
+  ip link set tap${n} master br0
+  ip link set dev tap${n} up
+  DHCP_HOSTS="${DHCP_HOSTS} --dhcp-host=52:55:00:d1:55:${n},192.168.66.1${n},node${n},infinite"
+  for s in $(seq 1 ${NUM_SECONDARY_NICS}); do
+    tap_name=stap$(($i - 1))-$(($s - 1))
+    ip tuntap add dev $tap_name mode tap user $(whoami)
+    ip link set $tap_name master br0
+    ip link set dev $tap_name up
+  done
+done
+
+# Make sure that all VMs can reach the internet
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i br0 -o eth0 -j ACCEPT
+
+exec dnsmasq -d ${DHCP_HOSTS} --dhcp-range=192.168.66.10,192.168.66.200,infinite
