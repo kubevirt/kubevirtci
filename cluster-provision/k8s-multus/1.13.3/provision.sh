df4e2a4315932c047ca5003f8da06097a9348abc
Félix Enrique Llorente Pastora <ellorent@redhat.com>
2020-04-08 13:15:53 +0200

    Delete the k8s-multus-1.13.3 provider (#339)
    It has being replaced by the "KUBEVIRT_WITH_CNAO" variable currently
    actionable at k8s-1.17 provider.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster-provision/k8s-multus/1.13.3/provision.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export version=1.13.3
-../provision.sh
--- cluster-provision/k8s-multus/scripts/logging.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-kubectl --kubeconfig /etc/kubernetes/admin.conf create namespace logging
-kubectl apply -f /tmp/logging.yaml
