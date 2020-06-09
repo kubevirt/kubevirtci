309d3eddbf3b0dd6396f565ef05d9fa4bcf51e54
Quique Llorente <ellorent@redhat.com>
2020-06-09 16:31:55 +0200

    Retrieve junit xml files
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- hack/conformance.sh
+++ hack/conformance.sh
@@ -1,15 +1,24 @@
-set -e
+set -xe
 
 ARTIFACTS=${ARTIFACTS:-${PWD}}
+sonobuoy_version=0.18.2
 
 if [[ -z "$KUBEVIRT_PROVIDER" ]]; then
     echo "KUBEVIRT_PROVIDER is not set" 1>&2
     exit 1
 fi
 
-curl -O -L https://github.com/vmware-tanzu/sonobuoy/releases/download/v0.18.2/sonobuoy_0.18.2_linux_amd64.tar.gz
-tar xf sonobuoy_0.18.2_linux_amd64.tar.gz
+export KUBECONFIG=$(cluster-up/kubeconfig.sh)
 
-export KUBECONFIG=_ci-configs/${KUBEVIRT_PROVIDER}/.kubeconfig
-trap "./sonobuoy status --json; ./sonobuoy logs > ${ARTIFACTS}/sonobuoy.log" EXIT
+teardown() {
+    ./sonobuoy status --json
+    ./sonobuoy logs > ${ARTIFACTS}/sonobuoy.log
+    results_tarball=$(./sonobuoy retrieve)
+    tar -xvzf $results_tarball plugins/e2e/results/
+    cp -f $(find plugins/e2e/results/* -name "*.xml") ${ARTIFACTS}/
+}
+
+curl -L https://github.com/vmware-tanzu/sonobuoy/releases/download/v${sonobuoy_version}/sonobuoy_${sonobuoy_version}_linux_amd64.tar.gz | tar -xz
+
+trap teardown EXIT
 ./sonobuoy run --wait
