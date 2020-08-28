64803e5dea14843801d951b6a91953bba975fe9e
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-08-28 10:32:35 +0200

    Add k8s 1.19 provider
    Create k8s 1.19 folder and files for provisioning.
    
    Use iptables instead of ipvs for now.
    
    Update sonobuoy config and version to 0.19.0, use different version
    per provider as given per config file conformance.json.
    
    Also add SONOBUOY_EXTRA_ARGS to be able to execute certain tests.
    
    See https://sonobuoy.io/docs/master/sonobuoy-config/ for details
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

---
+++ cluster-provision/k8s/1.19/nodes.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -ex
+
+# Ensure that hugepages are there
+cat /proc/meminfo | sed -e "s/ //g" | grep "HugePages_Total:64"
+
+timeout=30
+interval=5
+while ! hostnamectl  |grep Transient ; do
+    echo "Waiting for dhclient to set the hostname from dnsmasq"
+    sleep $interval
+    timeout=$(( $timeout - $interval ))
+    if [ $timeout -le 0 ]; then
+        exit 1
+    fi
+done
+
+# Wait for docker, else network might not be ready yet
+while [[ `systemctl status docker | grep active | wc -l` -eq 0 ]]
+do
+    sleep 2
+done
+
+if [ -f /etc/sysconfig/kubelet ]; then
+    # TODO use config file! this is deprecated
+    cat <<EOT >>/etc/sysconfig/kubelet
+KUBELET_EXTRA_ARGS=${KUBELET_EXTRA_ARGS} --feature-gates=CPUManager=true,IPv6DualStack=true --cpu-manager-policy=static --kube-reserved=cpu=500m --system-reserved=cpu=500m
+EOT
+else
+    cat <<EOT >>/etc/systemd/system/kubelet.service.d/09-kubeadm.conf
+Environment="KUBELET_CPUMANAGER_ARGS=--feature-gates=CPUManager=true,IPv6DualStack=true --cpu-manager-policy=static --kube-reserved=cpu=500m --system-reserved=cpu=500m"
+EOT
+sed -i 's/$KUBELET_EXTRA_ARGS/$KUBELET_EXTRA_ARGS $KUBELET_CPUMANAGER_ARGS/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+fi
+
+systemctl daemon-reload
+service kubelet restart
+kubelet_rc=$?
+if [[ $kubelet_rc -ne 0 ]]; then
+    rm -rf /var/lib/kubelet/cpu_manager_state
+    service kubelet restart
+fi
+
+kubeadm join --token abcdef.1234567890123456 192.168.66.101:6443 --ignore-preflight-errors=all --discovery-token-unsafe-skip-ca-verification=true
