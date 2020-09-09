5cb9e4fef642626f29860baf525ab8901c8c6dec
Daniel Hiller <daniel.hiller.1972@gmail.com>
2020-09-09 09:57:21 +0200

    Add k8s 1.19 provider (#434)
    * Add k8s 1.19 provider
    
    Create k8s 1.19 folder and files for provisioning.
    
    Use iptables instead of ipvs for now.
    
    Update sonobuoy config and version to 0.19.0, use different version
    per provider as given per config file conformance.json.
    
    Also add SONOBUOY_EXTRA_ARGS to be able to execute certain tests.
    
    See https://sonobuoy.io/docs/master/sonobuoy-config/ for details
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>
    
    * Update image sha for k8s-1.19 pushed latest changes
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>
    
    * Update provider.sh with new line, add docs
    
    Signed-off-by: Daniel Hiller <daniel.hiller.1972@gmail.com>

--- hack/conformance.sh
+++ hack/conformance.sh
@@ -3,23 +3,25 @@
 set -xeuo pipefail
 
 ARTIFACTS=${ARTIFACTS:-${PWD}}
-sonobuoy_version=0.18.2
 config_file=${1:-}
+sonobuoy_version=0.19.0
+[[ -f "$config_file" ]] && sonobuoy_version=$(jq -r '.Version' "$config_file" | grep -oE '[0-9\.]+')
 
 if [[ -z "$KUBEVIRT_PROVIDER" ]]; then
     echo "KUBEVIRT_PROVIDER is not set" 1>&2
     exit 1
 fi
 
-export KUBECONFIG=$(cluster-up/kubeconfig.sh)
+KUBECONFIG=$(cluster-up/kubeconfig.sh)
+export KUBECONFIG
 
 teardown() {
     rv=$?
     ./sonobuoy status --json
-    ./sonobuoy logs > ${ARTIFACTS}/sonobuoy.log
+    ./sonobuoy logs > "${ARTIFACTS}/sonobuoy.log"
     results_tarball=$(./sonobuoy retrieve)
-    tar -xvzf $results_tarball plugins/e2e/results/
-    cp -f $(find plugins/e2e/results/* -name "*.xml") ${ARTIFACTS}/
+    tar -xvzf "$results_tarball" plugins/e2e/results/
+    cp -f "$(find plugins/e2e/results/* -name "*.xml")" "${ARTIFACTS}/"
 
     if [ $rv -ne 0 ]; then
         echo "error found, exiting"
@@ -29,13 +31,13 @@ teardown() {
     passed=$(./sonobuoy status --json | jq  ' .plugins[] | select(."result-status" == "passed")'  | wc -l)
     failed=$(./sonobuoy status --json | jq  ' .plugins[] | select(."result-status" == "failed")'  | wc -l)
 
-    if [ $passed -eq 0 ] || [ $failed -ne 0 ]; then
+    if [ "$passed" -eq 0 ] || [ "$failed" -ne 0 ]; then
         echo "sonobuoy failed"
         exit 1
     fi
 }
 
-curl -L https://github.com/vmware-tanzu/sonobuoy/releases/download/v${sonobuoy_version}/sonobuoy_${sonobuoy_version}_linux_amd64.tar.gz | tar -xz
+curl -L "https://github.com/vmware-tanzu/sonobuoy/releases/download/v${sonobuoy_version}/sonobuoy_${sonobuoy_version}_linux_amd64.tar.gz" | tar -xz
 
 trap teardown EXIT
 
@@ -43,6 +45,11 @@ run_cmd="./sonobuoy run --wait"
 
 if [ "$config_file" != "" ]; then
     run_cmd+=" --config $config_file"
+fi
+
+SONOBUOY_EXTRA_ARGS=${SONOBUOY_EXTRA_ARGS:-}
+if [ -n "$SONOBUOY_EXTRA_ARGS" ]; then
+    run_cmd+=" ${SONOBUOY_EXTRA_ARGS}"
 fi
 
 $run_cmd
