62e6c60b2a7bcb0a5101ca0c9b4b4c644efff6b6
Roman Mohr <rmohr@redhat.com>
2020-10-08 15:11:03 +0200

    Basic publish script
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

---
+++ publish.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ex
+
+export KUBEVIRTCI_TAG=$(date +"%y%m%d%H%M")-$(git rev-parse --short HEAD)
+
+TARGET_REPO="kubevirtci"
+TARGET_GIT_REMOTE="https://kubevirt-bot@github.com/kubevirt/kubevirtci.git"
+
+# Build gocli
+(cd cluster-provision/gocli && make container)
+docker tag kubevirtci/gocli ${TARGET_REPO}/gocli:${KUBEVIRTCI_TAG}
+
+# Provision all base images
+(cd cluster-provision/centos8 && ./build.sh)
+
+# Provision all clusters
+for i in $(find cluster-provision/k8s/* -maxdepth 0 -type d -printf '%f\n'); do
+    cluster-provision/gocli/build/cli provision cluster-provision/k8s/$i
+    docker tag kubevirtci/k8s-$i ${TARGET_REPO}/k8s-$i:${KUBEVIRTCI_TAG}
+done
+
+# Push all images
+docker push ${TARGET_REPO}/gocli:${KUBEVIRTCI_TAG}
+for i in $(find cluster-provision/k8s/* -maxdepth 0 -type d -printf '%f\n'); do
+    docker push ${TARGET_REPO}/k8s-$i:${KUBEVIRTCI_TAG}
+done
+
+git config user.name "kubevirt-bot"
+git config user.email "rmohr+kubebot@redhat.com"
+git tag ${KUBEVIRTCI_TAG}
+git push ${TARGET_GIT_REMOTE} ${KUBEVIRTCI_TAG}
