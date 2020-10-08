c485ab78e8f1034da99c2a912c9fac9ab25cc7b5
Roman Mohr <rmohr@redhat.com>
2020-10-08 16:25:27 +0200

    Remove unsupported 1.16 k8s lane
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- cluster-provision/centos7/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-centos_version="$(cat $DIR/version | tr -d '\n')"
-
-docker build --build-arg centos_version=$centos_version . -t kubevirtci/centos7
