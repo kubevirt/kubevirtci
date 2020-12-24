fba93d6b31cf4522eeae74281709cdec6422e972
Roman Mohr <rmohr@redhat.com>
2020-12-24 12:11:17 +0100

    kubevirt-testing is no longer needed
    kubevirt/kubevirt is creating now the necessary images via bazel.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- cluster-provision/images/kubevirt-testing/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(
-    cd "$(dirname "$BASH_SOURCE[0]")"
-    pwd
-)"
-
-docker build -t kubevirtci/kubevirt-testing:latest -f ${SCRIPT_DIR}/Dockerfile ${SCRIPT_DIR}
--- cluster-provision/images/kubevirt-testing/publish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker tag kubevirtci/kubevirt-testing:latest docker.io/kubevirtci/kubevirt-testing:latest
-docker push docker.io/kubevirtci/kubevirt-testing:latest
