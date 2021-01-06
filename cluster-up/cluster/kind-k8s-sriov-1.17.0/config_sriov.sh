321fb1cbe263cedf302c4f4691eef9abedfed500
Or Mergi <ormergi@redhat.com>
2021-01-06 12:12:53 +0200

    config_sriov.sh: Add used PF's names annotation
    The SriovNetworkNodePolicy manifest patched with the PF's
    names that moved to the clsuter nodes netns.
    Now that sriov-operator deployment moved to another script
    it is necessary to pass PF names found by config_sriov.sh,
    to sriov_operator_deployment.sh which eventually patch
    SriovNetworkNodePolicy manifest.
    
    This commit create annotation on nodes that has PF
    attached that containes the PF names it uses.
    The annotation name is given by $NODE_PFS_ANNOTATION,
    and could be overrided.
    
    The annotation format is json stings array, where each
    string is the PF interface name, e.g: "[ "ens1", "ens2", ... ]".
    
    Doing so will improve the visability of the PF's that being
    used by cluster nodes.
    Its enables checking which PF's are being used by a node
    easily instead of relaying on env vars or common files.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>
    
    Conflicts:
    	cluster-up/cluster/kind-k8s-sriov-1.17.0/config_sriov.sh

--- cluster-up/cluster/kind-k8s-sriov-1.17.0/config_sriov.sh
+++ cluster-up/cluster/kind-k8s-sriov-1.17.0/config_sriov.sh
@@ -9,6 +9,9 @@ PF_COUNT_PER_NODE=${PF_COUNT_PER_NODE:-1}
 
 [ $PF_COUNT_PER_NODE -le 0 ] && echo "FATAL: PF_COUNT_PER_NODE must be a positive integer" >&2 && exit 1
 
+SRIOV_NODE_LABEL=${SRIOV_NODE_LABEL:-"sriov=true"}
+NODE_PFS_ANNOTATION=${NODE_PFS_ANNOTATION:-"node_pfs"}
+
 function move_sriov_pfs_netns_to_node {
   local -r node=$1
   local -r pf_count_per_node=$2
@@ -60,8 +63,11 @@ NODE_PFS=($(move_sriov_pfs_netns_to_node "$SRIOV_NODE" "$PF_COUNT_PER_NODE"))
 SRIOV_NODE_CMD="docker exec -it -d ${SRIOV_NODE}"
 ${SRIOV_NODE_CMD} mount -o remount,rw /sys     # kind remounts it as readonly when it starts, we need it to be writeable
 ${SRIOV_NODE_CMD} chmod 666 /dev/vfio/vfio
-_kubectl label node $SRIOV_NODE sriov=true
+_kubectl label node $SRIOV_NODE $SRIOV_NODE_LABEL
 
 for pf in "${NODE_PFS[@]}"; do
   docker exec $SRIOV_NODE bash -c "echo 0 > /sys/class/net/$pf/device/sriov_numvfs"
 done
+
+used_pfs_yaml_format_array=$(sed 's/ /, /g' <<< "[\"${NODE_PFS[@]}\"]")
+_kubectl annotate node $SRIOV_NODE "$NODE_PFS_ANNOTATION=$used_pfs_yaml_format_array"
