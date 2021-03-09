b956b0dbfa3b96093028f9c8f03ae4fa4f629f1e
Or Mergi <ormergi@redhat.com>
2021-03-09 12:05:40 +0200

    SRIOV provider: Add support for multiple nodes
    Currently SRIOV provider does not support creating
    a setup with more than one worker.
    
    In order to test Kubevirt SRIOV Live Migration
    We need SRIOV provider cluster with more than one node.
    
    This change enables creating SRIOV provider cluster
    with more than two nodes, where each node will have
    PF attached to its netns.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- cluster-up/cluster/kind-k8s-sriov-1.17.0/config_sriov.sh
+++ cluster-up/cluster/kind-k8s-sriov-1.17.0/config_sriov.sh
@@ -13,9 +13,6 @@ source ${SCRIPT_PATH}/sriov-components/sriov_components.sh
 
 CONFIGURE_VFS_SCRIPT_PATH="$SCRIPT_PATH/configure_vfs.sh"
 
-MASTER_NODE="${CLUSTER_NAME}-control-plane"
-WORKER_NODE_ROOT="${CLUSTER_NAME}-worker"
-
 SRIOV_COMPONENTS_NAMESPACE="sriov"
 SRIOV_NODE_LABEL_KEY="sriov_capable"
 SRIOV_NODE_LABEL_VALUE="true"
@@ -25,85 +22,139 @@ SRIOVDP_RESOURCE_NAME="sriov_net"
 VFS_DRIVER="vfio-pci"
 VFS_DRIVER_KMODULE="vfio_pci"
 
-function move_sriov_pfs_netns_to_node {
-  local -r node=$1
-  local -r pf_count_per_node=$2
-  local -r pid="$(docker inspect -f '{{.State.Pid}}' $node)"
-  local pf_array=()
+function prepare_node_netns() {
+  local -r node_name=$1
+  local -r node_pid=$(docker inspect -f '{{.State.Pid}}' "$node_name")
 
-  mkdir -p /var/run/netns/
-  ln -sf /proc/$pid/ns/net "/var/run/netns/$node"
+  # Docker does not create the required symlink for a container netns
+  # it perverts iplink from learning that container netns.
+  # Thus it is necessary to create symlink between the current
+  # worker node (container) netns to /var/run/netns (consumed by iplink)
+  # Now the node container netns named with the node name will be visible.
+  ln -sf "/proc/$node_pid/ns/net" "/var/run/netns/$node_name"
+}
 
+function move_pf_to_node_netns() {
+  local -r node_name=$1
+  local -r pf_name=$2
+
+  # Move PF to node network-namespace
+  ip link set "$pf_name" netns "$node_name"
+  # Ensure current PF is up
+  ip netns exec "$node_name" ip link set up dev "$pf_name"
+  ip netns exec "$node_name" ip link show
+}
+
+function get_pfs_names() {
   local -r sriov_pfs=( $(find /sys/class/net/*/device/sriov_numvfs) )
-  [ "${#sriov_pfs[@]}" -eq 0 ] && echo "FATAL: Could not find available sriov PFs" >&2 && return 1
+  [ "${#sriov_pfs[@]}" -eq 0 ] && echo "FATAL: Could not find available sriov PFs on host" >&2 && return 1
 
+  local pf_name
+  local pf_names=()
   for pf in "${sriov_pfs[@]}"; do
-    local pf_name="${pf%%/device/*}"
+    pf_name="${pf%%/device/*}"
     pf_name="${pf_name##*/}"
-
     if [ $(echo "${PF_BLACKLIST[@]}" | grep "${pf_name}") ]; then
       continue
     fi
 
-    # In case two clusters started at the same time, they might race on the same PF.
-    # The first will manage to assign the PF to its container, and the 2nd will just skip it
-    # and try the rest of the PFs available.
-    if ip link set "$pf_name" netns "$node"; then
-      if timeout 10s bash -c "until ip netns exec $node ip link show $pf_name > /dev/null; do sleep 1; done"; then
-        pf_array+=("$pf_name")
-        [ "${#pf_array[@]}" -eq "$pf_count_per_node" ] && break
-      fi
-    fi
+    pfs_names+=( $pf_name )
   done
 
-  [ "${#pf_array[@]}" -lt "$pf_count_per_node" ] && \
-    echo "FATAL: Not enough PFs allocated, PF_BLACKLIST (${PF_BLACKLIST[@]}), PF_COUNT_PER_NODE ${PF_COUNT_PER_NODE}" >&2 && \
-    return 1
-
-  echo "${pf_array[@]}"
+  echo "${pfs_names[@]}"
 }
 
-# The first worker needs to be handled specially as it has no ending number, and sort will not work
-# We add the 0 to it and we remove it if it's the candidate worker
-WORKER=$(_kubectl get nodes | grep $WORKER_NODE_ROOT | sed "s/\b$WORKER_NODE_ROOT\b/${WORKER_NODE_ROOT}0/g" | sort -r | awk 'NR==1 {print $1}')
-if [[ -z "$WORKER" ]]; then
-  SRIOV_NODE=$MASTER_NODE
-else
-  SRIOV_NODE=$WORKER
-fi
+function configure_nodes_sriov_pfs_and_vfs() {
+  local -r nodes_array=($1)
+  local -r pfs_names_array=($2)
+  local -r pf_count_per_node=$3
 
-# this is to remove the ending 0 in case the candidate worker is the first one
-if [[ "$SRIOV_NODE" == "${WORKER_NODE_ROOT}0" ]]; then
-  SRIOV_NODE=${WORKER_NODE_ROOT}
-fi
+  local -r config_vf_script=$(basename "$CONFIGURE_VFS_SCRIPT_PATH")
+  local pfs_to_move=()
+  local pfs_array_offset=0
+  local node_exec
 
-NODE_PFS=($(move_sriov_pfs_netns_to_node "$SRIOV_NODE" "$PF_COUNT_PER_NODE"))
-NODE_PF=$NODE_PFS
+  # 'iplink' learns which network namespaces there are by checking /var/run/netns
+  mkdir -p /var/run/netns
+  for node in "${nodes_array[@]}"; do
+    prepare_node_netns "$node"
 
-SRIOV_NODE_CMD="docker exec ${SRIOV_NODE}"
-NODE_PF_NUM_VFS=$(${SRIOV_NODE_CMD} cat /sys/class/net/$NODE_PF/device/sriov_totalvfs)
+    ## Move PF's to node netns
+    # Slice '$pfs_names_array' to have unique silce for each node
+    # with '$pf_count_per_node' PF's names
+    pfs_to_move=( "${pfs_names_array[@]:$pfs_array_offset:$pf_count_per_node}" )
+    echo "Moving '${pfs_to_move[*]}' PF's to '$node' netns"
+    for pf_name in "${pfs_to_move[@]}"; do
+      move_pf_to_node_netns "$node" "$pf_name"
+    done
+    # Increment the offset for next slice
+    pfs_array_offset=$((pfs_array_offset + pf_count_per_node))
 
-# KIND mounts sysfs as readonly, this script requires R/W access to sysfs
-${SRIOV_NODE_CMD} mount -o remount,rw /sys
-${SRIOV_NODE_CMD} chmod 666 /dev/vfio/vfio
+    # KIND mounts sysfs as read-only by default, remount as R/W"
+    node_exec="docker exec $node"
+    $node_exec mount -o remount,rw /sys
+    $node_exec chmod 666 /dev/vfio/vfio
 
-# Create and configure SRIOV Virtual Functions on SRIOV node
-docker cp "$CONFIGURE_VFS_SCRIPT_PATH" "$SRIOV_NODE:/"
-config_vf_script=$(basename "$CONFIGURE_VFS_SCRIPT_PATH")
-${SRIOV_NODE_CMD} bash -c "DRIVER=$VFS_DRIVER DRIVER_KMODULE=$VFS_DRIVER_KMODULE ./$config_vf_script"
+    # Create and configure SRIOV Virtual Functions on SRIOV node
+    docker cp "$CONFIGURE_VFS_SCRIPT_PATH" "$node:/"
+    $node_exec bash -c "DRIVER=$VFS_DRIVER DRIVER_KMODULE=$VFS_DRIVER_KMODULE ./$config_vf_script"
 
-_kubectl label node $SRIOV_NODE $SRIOV_NODE_LABEL
+    _kubectl label node $node $SRIOV_NODE_LABEL
+  done
+}
 
+function validate_nodes_sriov_allocatable_resource() {
+  local -r resource_name="$SRIOVDP_RESOURCE_PREFIX/$SRIOVDP_RESOURCE_NAME"
+  local -r sriov_nodes=$(_kubectl get nodes -l $SRIOV_NODE_LABEL -o custom-columns=:.metadata.name --no-headers)
+
+  local num_vfs
+  for sriov_node in $sriov_nodes; do
+    num_vfs=$(total_vfs_count_on_node "$sriov_node")
+    sriov_components::wait_allocatable_resource "$sriov_node" "$resource_name" "$num_vfs"
+  done
+}
+
+function total_vfs_count_on_node() {
+  local -r node_name=$1
+  local -r node_pid=$(docker inspect -f '{{.State.Pid}}' "$node_name")
+  local -r pfs_sriov_numvfs=( $(cat /proc/$node_pid/root/sys/class/net/*/device/sriov_numvfs) )
+  local total_vfs_on_node=0
+
+  for num_vfs in "${pfs_sriov_numvfs[@]}"; do
+    total_vfs_on_node=$((total_vfs_on_node + num_vfs))
+  done
+
+  echo "$total_vfs_on_node"
+}
+
+worker_nodes=($(_kubectl get nodes -l node-role.kubernetes.io/worker -o custom-columns=:.metadata.name --no-headers))
+worker_nodes_count=${#worker_nodes[@]}
+[ "$worker_nodes_count" -eq 0 ] && echo "FATAL: no worker nodes found" >&2 && exit 1
+
+pfs_names=($(get_pfs_names))
+pf_count="${#pfs_names[@]}"
+[ "$pf_count" -eq 0 ] && echo "FATAL: Could not find available sriov PF's" >&2 && exit 1
+
+total_pf_required=$((worker_nodes_count*PF_COUNT_PER_NODE))
+[ "$pf_count" -lt "$total_pf_required" ] && \
+  echo "FATAL: there are not enough PF's on the host, try to reduce PF_COUNT_PER_NODE
+  Worker nodes count: $worker_nodes_count
+  PF per node count:  $PF_COUNT_PER_NODE
+  Total PF count required:  $total_pf_required" >&2 && exit 1
+
+## Move SRIOV Physical Functions to worker nodes create VF's and configure their drivers
+configure_nodes_sriov_pfs_and_vfs "${worker_nodes[*]}" "${pfs_names[*]}" "$PF_COUNT_PER_NODE"
+
+## Deploy Multus and SRIOV components
 sriov_components::deploy_multus
 sriov_components::deploy \
-  "$NODE_PF" \
+  "${pfs_names[*]}" \
   "$VFS_DRIVER" \
   "$SRIOVDP_RESOURCE_PREFIX" "$SRIOVDP_RESOURCE_NAME" \
   "$SRIOV_NODE_LABEL_KEY" "$SRIOV_NODE_LABEL_VALUE"
 
-# Verify that sriov node has sriov VFs allocatable resource
-resource_name="$SRIOVDP_RESOURCE_PREFIX/$SRIOVDP_RESOURCE_NAME"
-sriov_components::wait_allocatable_resource "$SRIOV_NODE" "$resource_name" "$NODE_PF_NUM_VFS"
+# Verify that each sriov capable node has sriov VFs allocatable resource
+validate_nodes_sriov_allocatable_resource
 sriov_components::wait_pods_ready
 
 _kubectl get nodes
--- cluster-up/cluster/kind-k8s-sriov-1.17.0/provider.sh
+++ cluster-up/cluster/kind-k8s-sriov-1.17.0/provider.sh
@@ -20,24 +20,20 @@ function set_kind_params() {
 }
 
 function up() {
-    if [[ "$KUBEVIRT_NUM_NODES" -ne 2 ]]; then
-        echo 'SR-IOV cluster can be only started with 2 nodes'
-        exit 1
-    fi
-
     # print hardware info for easier debugging based on logs
     echo 'Available NICs'
     docker run --rm --cap-add=SYS_RAWIO quay.io/phoracek/lspci@sha256:0f3cacf7098202ef284308c64e3fc0ba441871a846022bb87d65ff130c79adb1 sh -c "lspci | egrep -i 'network|ethernet'"
     echo ""
 
     cp $KIND_MANIFESTS_DIR/kind.yaml ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/kind.yaml
-
     kind_up
 
     # remove the rancher.io kind default storageClass
     _kubectl delete sc standard
 
     ${KUBEVIRTCI_PATH}/cluster/$KUBEVIRT_PROVIDER/config_sriov.sh
+
+    echo "$KUBEVIRT_PROVIDER cluster '$CLUSTER_NAME' is ready"
 }
 
 set_kind_params
