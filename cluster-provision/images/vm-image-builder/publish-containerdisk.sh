9dd16717cb3574be8500efd482e183e0c9beeb8e
Roman Mohr <rmohr@redhat.com>
2021-04-21 11:14:01 +0200

    Simplify publish-containerdisk.sh
    Call publisch-containerdisk.sh by providing the image directory and the
    full target image name:
    
    ```
    publish-containerdisk.sh myimage quay.io/kubevirtci/myimage:mytag
    ```
    
    The script will take the latest build with `myimage:devel` and publish
    it.
    
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- cluster-provision/images/vm-image-builder/publish-containerdisk.sh
+++ cluster-provision/images/vm-image-builder/publish-containerdisk.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-export REGISTRY=${REGISTRY:-docker.io}
-export REPOSITORY=${REPOSITORY:-kubevirt}
+if [ "$#" -ne 2 ]; then
+    echo "Usage: publish-containerdisk.sh <image-directory> <image-target-name> "
+    echo "Run `publish-continerdisk.sh example quay.io/kubevirtci/example:mytag` to push the local `example:devel` image to `quay.io/kubevirtci/example:mytag`."
+fi
 
-full_image_tag="${REGISTRY}/${REPOSITORY}/${IMAGE_NAME}:${TAG}"
+export IMAGE_NAME=$1
+export TAG=devel
+export FULL_IMAGE_NAME=$2
 
-docker tag "${IMAGE_NAME}:${TAG}" "${full_image_tag}"
-docker push "${full_image_tag}"
+docker tag "${IMAGE_NAME}:${TAG}" "${FULL_IMAGE_NAME}"
+docker push "${FULL_IMAGE_NAME}"
