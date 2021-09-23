d1e1f8bbb8fca3c8c60312d5273b334e745b5887
Daniel Hiller <dhiller@redhat.com>
2021-09-23 13:12:44 +0200

    Delete ip-reconciler cronjob pods before and during loop
    We directly delete the job pods before and during the check.
    Also we improve documentation.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- cluster-provision/k8s/check-cluster-up.sh
+++ cluster-provision/k8s/check-cluster-up.sh
@@ -6,85 +6,100 @@ set -exuo pipefail
 make -C ../gocli container
 
 CI=${CI:-"false"}
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 RUN_KUBEVIRT_CONFORMANCE=${RUN_KUBEVIRT_CONFORMANCE:-"true"}
 
 provision_dir="$1"
 provider="${provision_dir}"
 
-function cleanup {
-  cd "$DIR" && cd ../..
-  make cluster-down
+function cleanup() {
+    cd "$DIR" && cd ../..
+    make cluster-down
+}
+
+function delete_ip_reconciler_pods() {
+    set +e
+    pods="$(${ksh} get pods -n default | grep 'ip-reconciler' | awk '{ print $1; }' | tr "\n" " ")"
+    set -e
+    [ -z "${pods}" ] && return
+    set +e
+    ${ksh} delete --ignore-not-found=true pods ${pods}
+    set -e
 }
 
 export KUBEVIRTCI_GOCLI_CONTAINER=quay.io/kubevirtci/gocli:latest
 # check cluster-up
 (
-  ksh="./cluster-up/kubectl.sh"
-  ssh="./cluster-up/ssh.sh"
-  cd "$DIR" && cd ../..
-  export KUBEVIRTCI_PROVISION_CHECK=1
-  export KUBEVIRT_PROVIDER="k8s-${provider}"
-  export KUBEVIRT_NUM_NODES=2
-  export KUBEVIRT_NUM_SECONDARY_NICS=2
-  export KUBEVIRT_WITH_CNAO=true
-  export KUBEVIRT_DEPLOY_ISTIO=true
-  if [[ $KUBEVIRT_PROVIDER =~ k8s-1\.1.* ]]; then
-    export KUBEVIRT_DEPLOY_ISTIO=false
-  fi
-  export KUBEVIRT_DEPLOY_PROMETHEUS=true
-  export KUBEVIRT_DEPLOY_PROMETHEUS_ALERTMANAGER=true
-  export KUBEVIRT_DEPLOY_GRAFANA=true
-  if [[ ($KUBEVIRT_PROVIDER =~ k8s-1\.1.*) || ($KUBEVIRT_PROVIDER =~ k8s-1.20) ]]; then
-    export KUBEVIRT_DEPLOY_PROMETHEUS=false
-    export KUBEVIRT_DEPLOY_PROMETHEUS_ALERTMANAGER=false
-    export KUBEVIRT_DEPLOY_GRAFANA=false
-  fi
-  export KUBEVIRT_DEPLOY_CDI=true
-  trap cleanup EXIT ERR SIGINT SIGTERM SIGQUIT
-  bash -x ./cluster-up/up.sh
-  # This is required so that all the completed pods from ip-reconciler are getting removed directly
-  ${ksh} patch cronjob ip-reconciler --type merge -p '{ "spec": { "jobTemplate": { "spec": { "ttlSecondsAfterFinished": 0 } } } }'
-  timeout 210s bash -c "until ${ksh} wait --for=condition=Ready pod --timeout=30s --all; do sleep 1; done"
-  timeout 210s bash -c "until ${ksh} wait --for=condition=Ready pod --timeout=30s -n kube-system --all; do sleep 1; done"
-  ${ksh} get nodes
-  ${ksh} get pods -A
+    ksh="./cluster-up/kubectl.sh"
+    ssh="./cluster-up/ssh.sh"
+    cd "$DIR" && cd ../..
+    export KUBEVIRTCI_PROVISION_CHECK=1
+    export KUBEVIRT_PROVIDER="k8s-${provider}"
+    export KUBEVIRT_NUM_NODES=2
+    export KUBEVIRT_NUM_SECONDARY_NICS=2
+    export KUBEVIRT_WITH_CNAO=true
+    export KUBEVIRT_DEPLOY_ISTIO=true
+    if [[ $KUBEVIRT_PROVIDER =~ k8s-1\.1.* ]]; then
+        export KUBEVIRT_DEPLOY_ISTIO=false
+    fi
+    export KUBEVIRT_DEPLOY_PROMETHEUS=true
+    export KUBEVIRT_DEPLOY_PROMETHEUS_ALERTMANAGER=true
+    export KUBEVIRT_DEPLOY_GRAFANA=true
+    if [[ ($KUBEVIRT_PROVIDER =~ k8s-1\.1.*) || ($KUBEVIRT_PROVIDER =~ k8s-1.20) ]]; then
+        export KUBEVIRT_DEPLOY_PROMETHEUS=false
+        export KUBEVIRT_DEPLOY_PROMETHEUS_ALERTMANAGER=false
+        export KUBEVIRT_DEPLOY_GRAFANA=false
+    fi
+    export KUBEVIRT_DEPLOY_CDI=true
+    trap cleanup EXIT ERR SIGINT SIGTERM SIGQUIT
+    bash -x ./cluster-up/up.sh
 
-  # Run some checks for KUBEVIRT_NUM_NODES
-  # and KUBEVIRT_NUM_SECONDARY_NICS
-  ${ksh} get node node01
-  ${ksh} get node node02
-  ${ssh} node01 -- ip l show eth1
-  ${ssh} node01 -- ip l show eth2
-  ${ssh} node02 -- ip l show eth1
-  ${ssh} node02 -- ip l show eth2
+    # whereabouts ip-reconciler cleanup: ip-reconciler cronjob pods must get removed directly after creation
+    ${ksh} patch cronjob ip-reconciler --type merge -p '{ "spec": { "jobTemplate": { "spec": { "ttlSecondsAfterFinished": 0 } } } }'
 
-  pre_pull_image_file="$DIR/${provision_dir}/extra-pre-pull-images"
-  if [ -f "${pre_pull_image_file}" ]; then
-    bash -x "$DIR/deploy-manifests.sh" "${provision_dir}"
-    bash -x "$DIR/check-pod-images.sh" "${provision_dir}"
-    bash -x "$DIR/validate-pod-pull-policies.sh"
-  fi
+    # whereabouts ip-reconciler cleanup: delete eventually lingering pods before and during the loop
+    delete_ip_reconciler_pods
+    until ${ksh} wait --for=condition=Ready pod --timeout=30s --all; do
+        delete_ip_reconciler_pods
+        sleep 1
+    done
+    timeout 210s bash -c "until ${ksh} wait --for=condition=Ready pod --timeout=30s -n kube-system --all; do sleep 1; done"
+    ${ksh} get nodes
+    ${ksh} get pods -A
 
-  # Run conformance test only at CI and if the provider has them activated
-  conformance_config=$DIR/${provision_dir}/conformance.json
+    # Run some checks for KUBEVIRT_NUM_NODES
+    # and KUBEVIRT_NUM_SECONDARY_NICS
+    ${ksh} get node node01
+    ${ksh} get node node02
+    ${ssh} node01 -- ip l show eth1
+    ${ssh} node01 -- ip l show eth2
+    ${ssh} node02 -- ip l show eth1
+    ${ssh} node02 -- ip l show eth2
 
-  if [ "${CI}" == "true" -a -f $conformance_config ]; then
-    sonobouy_plugins=""
-
-    if [ "$RUN_KUBEVIRT_CONFORMANCE" == "true" ]; then
-      LATEST=$(curl -L "https://storage.googleapis.com/kubevirt-prow/devel/nightly/release/kubevirt/kubevirt/latest")
-      ${ksh} apply -f "https://storage.googleapis.com/kubevirt-prow/devel/nightly/release/kubevirt/kubevirt/${LATEST}/kubevirt-operator.yaml"
-      ${ksh} apply -f "https://storage.googleapis.com/kubevirt-prow/devel/nightly/release/kubevirt/kubevirt/${LATEST}/kubevirt-cr.yaml"
-
-      ${ksh} wait -n kubevirt kv kubevirt --for condition=Available --timeout 15m
-
-      export SONOBUOY_EXTRA_ARGS="--plugin https://storage.googleapis.com/kubevirt-prow/devel/nightly/release/kubevirt/kubevirt/${LATEST}/conformance.yaml"
-      hack/conformance.sh $conformance_config
+    pre_pull_image_file="$DIR/${provision_dir}/extra-pre-pull-images"
+    if [ -f "${pre_pull_image_file}" ]; then
+        bash -x "$DIR/deploy-manifests.sh" "${provision_dir}"
+        bash -x "$DIR/check-pod-images.sh" "${provision_dir}"
+        bash -x "$DIR/validate-pod-pull-policies.sh"
     fi
 
-    export SONOBUOY_EXTRA_ARGS="--plugin systemd-logs --plugin e2e"
-    hack/conformance.sh $conformance_config
-  fi
+    # Run conformance test only at CI and if the provider has them activated
+    conformance_config=$DIR/${provision_dir}/conformance.json
+
+    if [ "${CI}" == "true" ] && [ -f $conformance_config ]; then
+        if [ "$RUN_KUBEVIRT_CONFORMANCE" == "true" ]; then
+            LATEST=$(curl -L "https://storage.googleapis.com/kubevirt-prow/devel/nightly/release/kubevirt/kubevirt/latest")
+            ${ksh} apply -f "https://storage.googleapis.com/kubevirt-prow/devel/nightly/release/kubevirt/kubevirt/${LATEST}/kubevirt-operator.yaml"
+            ${ksh} apply -f "https://storage.googleapis.com/kubevirt-prow/devel/nightly/release/kubevirt/kubevirt/${LATEST}/kubevirt-cr.yaml"
+
+            ${ksh} wait -n kubevirt kv kubevirt --for condition=Available --timeout 15m
+
+            export SONOBUOY_EXTRA_ARGS="--plugin https://storage.googleapis.com/kubevirt-prow/devel/nightly/release/kubevirt/kubevirt/${LATEST}/conformance.yaml"
+            hack/conformance.sh $conformance_config
+        fi
+
+        export SONOBUOY_EXTRA_ARGS="--plugin systemd-logs --plugin e2e"
+        hack/conformance.sh $conformance_config
+    fi
 )
