97c4fa558c402bc20359814d7acfed8ea0affd9d
Enrique Llorente <ellorent@redhat.com>
2022-03-31 14:43:23 +0200

    alpine: Support multiple architectures
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- cluster-provision/images/vm-image-builder/alpine-cloud-init/create-image.sh
+++ cluster-provision/images/vm-image-builder/alpine-cloud-init/create-image.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
-set -exuo pipefail
+set -ex
 
-curl -L  https://raw.githubusercontent.com/alpinelinux/alpine-make-vm-image/master/alpine-make-vm-image -o alpine-make-vm-image
-chmod 755 alpine-make-vm-image
+if [ "${ARCHITECTURE}" != ""  ]; then
+    PLATFORM=linux/$ARCHITECTURE
+fi
 
-./alpine-make-vm-image --image-format qcow2 --image-size 5G \
-    --repositories-file repositories \
-    --packages "$(cat packages)" \
-    --serial-console \
-    --script-chroot $1 -- configure.sh
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+if [ ! -f alpine-make-vm-image ]; then
+    curl  https://raw.githubusercontent.com/alpinelinux/alpine-make-vm-image/master/alpine-make-vm-image -o alpine-make-vm-image
+    chmod 755 alpine-make-vm-image
+fi
+
+docker run --platform=$PLATFORM --privileged -v $(pwd):$(pwd):z alpine ash -c "cd $(pwd) &&
+./alpine-make-vm-image \
+	--image-format qcow2 \
+	--image-size 5G \
+	--repositories-file repositories \
+	--packages \"$(cat packages)\" \
+	--serial-console \
+	--script-chroot \
+	$1 -- configure.sh"
