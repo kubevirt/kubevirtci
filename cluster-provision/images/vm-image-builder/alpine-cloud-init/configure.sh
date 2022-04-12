5af2bf4bb2b0a7703240b6e5a72975e9e4ce7a66
Enrique Llorente Pastora <ellorent@redhat.com>
2022-04-12 08:14:05 +0200

    alpine: Reduce size (#776)
    The alpine qcow2 image can be reduced by reseving less space for the
    disk.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- cluster-provision/images/vm-image-builder/alpine-cloud-init/configure.sh
+++ cluster-provision/images/vm-image-builder/alpine-cloud-init/configure.sh
@@ -6,15 +6,6 @@ step() {
 	printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
 }
 
-step 'Set up qemu-guest-agent'
-cat > /etc/conf.d/qemu-guest-agent <<-EOF
-GA_METHOD="virtio-serial"
-GA_PATH="/dev/vport1p1"
-EOF
-
-step 'Set up timezone'
-setup-timezone -z Europe/Prague
-
 step 'Adjust rc.conf'
 sed -Ei \
 	-e 's/^[# ](rc_depend_strict)=.*/\1=NO/' \
@@ -23,9 +14,4 @@ sed -Ei \
 	/etc/rc.conf
 
 step 'Enable services'
-rc-update add acpid default
-rc-update add chronyd default
-rc-update add crond default
-rc-update add termencoding boot
-rc-update add qemu-guest-agent default
 rc-update add cloud-init default
