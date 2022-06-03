0da8683d095be41d974b839ac65d4f53383ce382
Brian Carey <bcarey@redhat.com>
2022-06-03 12:49:26 +0100

    Add kubernetes 1.24 provider for kubevirtci (#800)
    * Run bazel run //robots/cmd/kubevirtci-bumper:kubevirtci-bumper -- -ensure-latest --k8s-provider-dir /home/prow/go/src/github.com/kubevirt/project-infra/../kubevirtci/cluster-provision/k8s --cluster-up-dir /home/prow/go/src/github.com/kubevirt/project-infra/../kubevirtci/cluster-up/cluster
    
    Signed-off-by: kubevirt-bot <kubevirtbot@redhat.com>
    
    * Update kubeadm.conf to v1beta3 and add toleration to cni
    
    Remove IPv6DualStack feature gates from kubeadm.conf as IPv6 Dual stack
    is now default.
    
    Remove control-plane NoSchedule taint from node01
    
    This taint has to be removed to allow scheduling test pods/VMs to node01
    
    Clean up of starting cri-o and kubelet services
    
    Move cni taint toleration to cni.diff
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>
    
    * Bump CNAO to v0.76.0 for k8s-1.24
    
    The older CNAO version that is being used by the previous providers did
    not have tolerations for the taint node-role.kubernetes.io/control-plane:NoSchedule
    - the related pods were not getting scheduled.
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>
    
    * Update to version 1.24 of cri-o
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>
    
    * Remove dns:type from kubeadm.conf
    
    DNS type has been removed from the kubeadm configuration v1beta3 spec[1]
    
    [1] https://kubernetes.io/docs/reference/config-api/kubeadm-config.v1beta3/#kubeadm-k8s-io-v1beta3-DNS
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>
    
    * Increase KUBEVIRT_MEMORY_SIZE to 5520M
    
    Some conformance tests were failing due to a virt-launcher pod not being
    scheduled. The pod remained in pending state because of insufficient memory on the worker node
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>
    
    Co-authored-by: kubevirt-bot <kubevirtbot@redhat.com>

---
+++ cluster-provision/k8s/1.24/provision.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+set -ex
+
+if [ ! -f "/tmp/extra-pre-pull-images" ]; then
+    echo "ERROR: extra-pre-pull-images list missing"
+    exit 1
+fi
+if [ ! -f "/tmp/fetch-images.sh" ]; then
+    echo "ERROR: fetch-images.sh missing"
+    exit 1
+fi
+
+KUBEVIRTCI_SHARED_DIR=/var/lib/kubevirtci
+mkdir -p $KUBEVIRTCI_SHARED_DIR
+cat << EOF > $KUBEVIRTCI_SHARED_DIR/shared_vars.sh
+#!/bin/bash
+set -ex
+export KUBELET_CGROUP_ARGS="--cgroup-driver=systemd --runtime-cgroups=/systemd/system.slice --kubelet-cgroups=/systemd/system.slice"
+export KUBELET_FEATURE_GATES="IPv6DualStack=true"
+export ISTIO_VERSION=1.10.0
+export ISTIO_BIN_DIR=/opt/istio-$ISTIO_VERSION/bin
+EOF
+source $KUBEVIRTCI_SHARED_DIR/shared_vars.sh
+
+function pull_container_retry() {
+    retry=0
+    maxRetries=5
+    retryAfterSeconds=3
+    until [ ${retry} -ge ${maxRetries} ]; do
+        podman pull "$@" && break
+        retry=$((${retry} + 1))
+        echo "Retrying ${FUNCNAME[0]} [${retry}/${maxRetries}] in ${retryAfterSeconds}(s)"
+        sleep ${retryAfterSeconds}
+    done
+
+    if [ ${retry} -ge ${maxRetries} ]; then
+        echo "${FUNCNAME[0]} Failed after ${maxRetries} attempts!"
+        exit 1
+    fi
+}
+
+# Install modules of the initrd kernel
+dnf install -y "kernel-modules-$(uname -r)"
+
+# Resize root partition
+dnf install -y cloud-utils-growpart
+if growpart /dev/vda 1; then
+    xfs_growfs -d /
+fi
+
+dnf install -y patch
+
+systemctl stop firewalld || :
+systemctl disable firewalld || :
+# Make sure the firewall is never enabled again
+# Enabling the firewall destroys the iptable rules
+yum -y remove firewalld
+
+# Required for iscsi demo to work.
+yum -y install iscsi-initiator-utils
+
+# for rook ceph
+dnf -y install lvm2
+# Convince ceph our storage is fast (not a rotational disk)
+echo 'ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="vd[a-z]", ATTR{queue/rotational}="0"' \
+	> /etc/udev/rules.d/60-force-ssd-rotational.rules
+
+# To prevent preflight issue related to tc not found
+dnf install -y tc
+
+# Install istioctl
+export PATH=$ISTIO_BIN_DIR:$PATH
+(
+  set -E
+  mkdir -p $ISTIO_BIN_DIR
+  curl https://storage.googleapis.com/kubevirtci-istioctl-mirror/istio-$ISTIO_VERSION/bin/istioctl -o $ISTIO_BIN_DIR/istioctl
+  chmod +x $ISTIO_BIN_DIR/istioctl
+)
+# generate Istio manifests for pre-pulling images
+istioctl manifest generate --set profile=demo --set components.cni.enabled=true | tee /tmp/istio-deployment.yaml
+
+export CRIO_VERSION=1.24
+cat << EOF >/etc/yum.repos.d/devel_kubic_libcontainers_stable.repo
+[devel_kubic_libcontainers_stable]
+name=Stable Releases of Upstream github.com/containers packages (CentOS_8_Stream)
+type=rpm-md
+baseurl=https://storage.googleapis.com/kubevirtci-crio-mirror/devel_kubic_libcontainers_stable/
+gpgcheck=0
+enabled=1
+EOF
+cat << EOF >/etc/yum.repos.d/devel_kubic_libcontainers_stable_cri-o_${CRIO_VERSION}.repo
+[devel_kubic_libcontainers_stable_cri-o_${CRIO_VERSION}]
+name=devel:kubic:libcontainers:stable:cri-o:${CRIO_VERSION} (CentOS_8_Stream)
+type=rpm-md
+baseurl=https://storage.googleapis.com/kubevirtci-crio-mirror/devel_kubic_libcontainers_stable_cri-o_${CRIO_VERSION}
+gpgcheck=0
+enabled=1
+EOF
+dnf install -y cri-o
+
+systemctl enable --now crio
+
+# install podman for functionality missing in crictl (tag, etc)
+dnf install -y podman
+dnf install -y libseccomp-devel
+
+# link docker to podman as we need docker in test repos to pre-pull images
+# don't break them by doing a symlink
+ln -s /usr/bin/podman /usr/bin/docker
+
+cat << EOF > /etc/containers/registries.conf
+[registries.search]
+registries = ['registry.access.redhat.com', 'registry.fedoraproject.org', 'quay.io', 'docker.io']
+
+[registries.insecure]
+registries = ['registry:5000']
+
+[registries.block]
+registries = []
+EOF
+
+#TODO: el8 repo
+# Add Kubernetes repository.
+cat <<EOF >/etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Install Kubernetes packages.
+dnf install --skip-broken --nobest --nogpgcheck --disableexcludes=kubernetes -y \
+    kubeadm-${version} \
+    kubelet-${version} \
+    kubectl-${version} \
+    kubernetes-cni
+
+kubeadm config images pull --kubernetes-version ${version}
+
+dnf install -y centos-release-nfv-openvswitch
+dnf install -y openvswitch2.16
+
+mkdir -p /provision
+cni_manifest="/provision/cni.yaml"
+mv /tmp/cni.do-not-change.yaml $cni_manifest
+patch $cni_manifest /tmp/cni.diff
+
+
+# Pre pull all images from the manifests
+for image in $(/tmp/fetch-images.sh /tmp); do
+    pull_container_retry "${image}"
+done
+
+# Pre pull additional images from list
+for image in $(cat "/tmp/extra-pre-pull-images"); do
+    pull_container_retry "${image}"
+done
