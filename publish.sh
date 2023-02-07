317da6491843871e7d30d04055a38345156f3854
Or Shoval <oshoval@redhat.com>
2023-02-07 10:46:03 +0200

    publish.sh: Cosmetics
    Cleanup and reorder, prepare for splitting to functions.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- publish.sh
+++ publish.sh
@@ -2,8 +2,7 @@
 
 set -ex
 
-KUBEVIRTCI_TAG=$(date +"%y%m%d%H%M")-$(git rev-parse --short HEAD)
-export KUBEVIRTCI_TAG
+export KUBEVIRTCI_TAG=$(date +"%y%m%d%H%M")-$(git rev-parse --short HEAD)
 
 TARGET_REPO="quay.io/kubevirtci"
 TARGET_KUBEVIRT_REPO="quay.io/kubevirt"
@@ -27,32 +26,28 @@ for i in ${CLUSTERS}; do
     docker tag ${TARGET_REPO}/k8s-$i ${TARGET_REPO}/k8s-$i:${KUBEVIRTCI_TAG}-slim
 done
 
-# Provision alpine container disk and tag it
-(cd cluster-provision/images/vm-image-builder && ./create-containerdisk.sh alpine-cloud-init)
-docker tag  alpine-cloud-init:devel ${TARGET_REPO}/alpine-with-test-tooling-container-disk:${KUBEVIRTCI_TAG}
-docker tag  alpine-cloud-init:devel ${TARGET_KUBEVIRT_REPO}/alpine-with-test-tooling-container-disk:devel
-
 # Push all images
-
 # until "unknown blob" issue is fixed use skopeo to push image
 # see https://github.com/moby/moby/issues/43234
-
 IMAGES="${CLUSTERS}"
 TARGET_IMAGE="${TARGET_REPO}/gocli:${KUBEVIRTCI_TAG}"
 skopeo copy "docker-daemon:${TARGET_IMAGE}" "docker://${TARGET_IMAGE}"
-#docker push ${TARGET_IMAGE}
 for i in ${IMAGES}; do
     TARGET_IMAGE="${TARGET_REPO}/k8s-$i:${KUBEVIRTCI_TAG}"
     skopeo copy "docker-daemon:${TARGET_IMAGE}" "docker://${TARGET_IMAGE}"
-    #docker push ${TARGET_IMAGE}
 
     TARGET_IMAGE="${TARGET_REPO}/k8s-$i:${KUBEVIRTCI_TAG}-slim"
     skopeo copy "docker-daemon:${TARGET_IMAGE}" "docker://${TARGET_IMAGE}"
 done
 
+# Provision alpine container disk and tag it
+(cd cluster-provision/images/vm-image-builder && ./create-containerdisk.sh alpine-cloud-init)
+docker tag alpine-cloud-init:devel ${TARGET_REPO}/alpine-with-test-tooling-container-disk:${KUBEVIRTCI_TAG}
+docker tag alpine-cloud-init:devel ${TARGET_KUBEVIRT_REPO}/alpine-with-test-tooling-container-disk:devel
+
+# Push alpine container disk
 TARGET_IMAGE="${TARGET_REPO}/alpine-with-test-tooling-container-disk:${KUBEVIRTCI_TAG}"
 skopeo copy "docker-daemon:${TARGET_IMAGE}" "docker://${TARGET_IMAGE}"
-#docker push ${TARGET_IMAGE}
 TARGET_KUBEVIRT_IMAGE="${TARGET_KUBEVIRT_REPO}/alpine-with-test-tooling-container-disk:devel"
 skopeo copy "docker-daemon:${TARGET_KUBEVIRT_IMAGE}" "docker://${TARGET_KUBEVIRT_IMAGE}"
 
