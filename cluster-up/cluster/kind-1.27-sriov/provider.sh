36501b64e7f69c22516b047e49cc337753a1a89f
Or Shoval <oshoval@redhat.com>
2023-06-05 09:52:38 +0300

    kind, SR-IOV: Arrange to functions
    In order to ease readability, arrange to functions.
    Add a flag DEPLOY_SRIOV (default: true), in order to allow
    skipping deploying SR-IOV.
    This is turn would allow to spin a kind cluster, and use it
    for non SR-IOV tasks.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- cluster-up/cluster/kind-1.27-sriov/provider.sh
+++ cluster-up/cluster/kind-1.27-sriov/provider.sh
@@ -6,12 +6,25 @@ DEFAULT_CLUSTER_NAME="sriov"
 DEFAULT_HOST_PORT=5000
 ALTERNATE_HOST_PORT=5001
 export CLUSTER_NAME=${CLUSTER_NAME:-$DEFAULT_CLUSTER_NAME}
+export DEPLOY_SRIOV=${DEPLOY_SRIOV:-true}
 
-if [ $CLUSTER_NAME == $DEFAULT_CLUSTER_NAME ]; then
-    export HOST_PORT=$DEFAULT_HOST_PORT
+if [ $DEPLOY_SRIOV == true ]; then
+    if [ $CLUSTER_NAME == $DEFAULT_CLUSTER_NAME ]; then
+        export HOST_PORT=$DEFAULT_HOST_PORT
+    else
+        export HOST_PORT=$ALTERNATE_HOST_PORT
+    fi
 else
-    export HOST_PORT=$ALTERNATE_HOST_PORT
+    export HOST_PORT=$DEFAULT_HOST_PORT
+    CLUSTER_NAME="kind"
 fi
+
+function print_available_nics() {
+    # print hardware info for easier debugging based on logs
+    echo 'Available NICs'
+    ${CRI_BIN} run --rm --cap-add=SYS_RAWIO quay.io/phoracek/lspci@sha256:0f3cacf7098202ef284308c64e3fc0ba441871a846022bb87d65ff130c79adb1 sh -c "lspci | egrep -i 'network|ethernet'"
+    echo ""
+}
 
 function set_kind_params() {
     export KIND_VERSION="${KIND_VERSION:-0.18.0}"
@@ -44,12 +57,13 @@ function configure_registry_proxy() {
     KIND_BIN="$kind_binary_path" PROXY_HOSTNAME="$ci_proxy_hostname" $configure_registry_proxy_script
 }
 
-function up() {
-    # print hardware info for easier debugging based on logs
-    echo 'Available NICs'
-    ${CRI_BIN} run --rm --cap-add=SYS_RAWIO quay.io/phoracek/lspci@sha256:0f3cacf7098202ef284308c64e3fc0ba441871a846022bb87d65ff130c79adb1 sh -c "lspci | egrep -i 'network|ethernet'"
-    echo ""
+function deploy_sriov() {
+    print_available_nics
+    ${KUBEVIRTCI_PATH}/cluster/$KUBEVIRT_PROVIDER/config_sriov_cluster.sh
+    print_sriov_data
+}
 
+function up() {
     cp $KIND_MANIFESTS_DIR/kind.yaml ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/kind.yaml
     export CONFIG_WORKER_CPU_MANAGER=true
     kind_up
@@ -59,9 +73,7 @@ function up() {
     # remove the rancher.io kind default storageClass
     _kubectl delete sc standard
 
-    ${KUBEVIRTCI_PATH}/cluster/$KUBEVIRT_PROVIDER/config_sriov_cluster.sh
-
-    print_sriov_data
+    [ $DEPLOY_SRIOV == true ] && deploy_sriov
     echo "$KUBEVIRT_PROVIDER cluster '$CLUSTER_NAME' is ready"
 }
 
