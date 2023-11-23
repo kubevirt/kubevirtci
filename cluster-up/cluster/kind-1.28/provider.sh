5a61c0f526d7dcc5e7bcade6d773f420669d1db1
howard zhang <howard.zhang@arm.com>
2023-11-23 14:45:00 +0800

    kind: update from kind-1.27 to kind-1.28
    As the node image kindest/node:v1.27.3
    does not contain the PR https://github.com/kubernetes/kubernetes/pull/118635
    And it leads to some device errors for virt-launcher after restart kubelet.
    We update the node image from v1.27.3 to v1.28.0
    
    Signed-off-by: howard zhang <howard.zhang@arm.com>

--- cluster-up/cluster/kind-1.28/provider.sh
+++ cluster-up/cluster/kind-1.28/provider.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DEFAULT_CLUSTER_NAME="kind-1.27"
+DEFAULT_CLUSTER_NAME="kind-1.28"
 DEFAULT_HOST_PORT=5000
 ALTERNATE_HOST_PORT=5001
 export CLUSTER_NAME=${CLUSTER_NAME:-$DEFAULT_CLUSTER_NAME}
@@ -15,7 +15,7 @@ fi
 
 function set_kind_params() {
     export KIND_VERSION="${KIND_VERSION:-0.20.0}"
-    export KIND_NODE_IMAGE="${KIND_NODE_IMAGE:-kindest/node:v1.27.3@sha256:3966ac761ae0136263ffdb6cfd4db23ef8a83cba8a463690e98317add2c9ba72}"
+    export KIND_NODE_IMAGE="${KIND_NODE_IMAGE:-kindest/node:v1.28.0@sha256:b7a4cad12c197af3ba43202d3efe03246b3f0793f162afb40a33c923952d5b31}"
     export KUBECTL_PATH="${KUBECTL_PATH:-/usr/bin/kubectl}"
 }
 
