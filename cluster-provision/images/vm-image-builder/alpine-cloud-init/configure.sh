eb91f85949396d8e0cafa392ed66ec566e6707dc
Alex Kalenyuk <akalenyu@redhat.com>
2024-04-07 11:04:56 +0300

    Add networking service to alpine start
    Otherwise, the network interfaces will not be brought up:
    https://github.com/kubevirt/kubevirt/pull/11256#issuecomment-2039359831
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- cluster-provision/images/vm-image-builder/alpine-cloud-init/configure.sh
+++ cluster-provision/images/vm-image-builder/alpine-cloud-init/configure.sh
@@ -25,4 +25,5 @@ rc-update add cloud-init default
 rc-update add cloud-init-local default
 rc-update add cloud-config default
 rc-update add cloud-final default
+rc-update add networking default
 rc-update add sshd default
