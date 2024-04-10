ef69592298e2e1e93995fd1e442c6a86e0ef3ea0
Alex Kalenyuk <akalenyu@redhat.com>
2024-04-10 22:24:04 +0300

    Bump alpine to 3.19 (#1167)
    * Bump alpine to 3.19
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>
    
    * Add networking service to alpine start
    
    Otherwise, the network interfaces will not be brought up:
    https://github.com/kubevirt/kubevirt/pull/11256#issuecomment-2039359831
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>
    
    ---------
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- cluster-provision/images/vm-image-builder/alpine-cloud-init/configure.sh
+++ cluster-provision/images/vm-image-builder/alpine-cloud-init/configure.sh
@@ -22,6 +22,8 @@ sed -Ei \
 step 'Enable services'
 rc-update add qemu-guest-agent default
 rc-update add cloud-init default
+rc-update add cloud-init-local default
 rc-update add cloud-config default
 rc-update add cloud-final default
+rc-update add networking default
 rc-update add sshd default
