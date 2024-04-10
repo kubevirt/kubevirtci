921e38db0d0de0540ada1c778f0154cafd43d982
bmordeha <bmordeha@redhat.com>
2024-04-10 12:28:35 +0300

    Add AAQ Operator as an optional cluster addon AAQ Operator will be disabled by default To install AAQ set KUBEVIRT_DEPLOY_AAQ to true to use custom version set KUBEVIRT_CUSTOM_AAQ_VERSION with the desired version.
    Signed-off-by: bmordeha <bmordeha@redhat.com>

--- cluster-provision/k8s/1.30/k8s_provision.sh
+++ cluster-provision/k8s/1.30/k8s_provision.sh
@@ -359,6 +359,9 @@ cp -rf /tmp/multus /opt/
 # copy cdi manifests
 cp -rf /tmp/cdi*.yaml /opt/
 
+# copy aaq manifests
+cp -rf /tmp/aaq/ /opt/
+
 # Create a properly labelled tmp directory for testing
 mkdir -p /var/provision/kubevirt.io/tests
 chcon -t container_file_t /var/provision/kubevirt.io/tests
