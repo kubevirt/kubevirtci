075a40f0ea4ce9f3b12a7e2ded8220707f73a9a5
Lee Yarwood <lyarwood@redhat.com>
2024-04-12 10:55:17 +0100

    Introduce KUBEVIRT_CPU_MANAGER_POLICY
    This change introduces the KUBEVIRT_CPU_MANAGER_POLICY env var that
    allows a user to enable the CPUManager with a specific policy.
    
    The static policy is the only supported policy at present with the
    full-pcpus-only option provided.
    
    Two FIXMEs are left in the code to move to config drop-ins and fix an
    issue with HereDocs that could clean up this implementation slightly.
    
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- cluster-up/hack/common.sh
+++ cluster-up/hack/common.sh
@@ -39,6 +39,7 @@ KUBEVIRT_CUSTOM_CDI_VERSION=${KUBEVIRT_CUSTOM_CDI_VERSION}
 KUBEVIRT_SWAP_ON=${KUBEVIRT_SWAP_ON:-false}
 KUBEVIRT_KSM_ON=${KUBEVIRT_KSM_ON:-false}
 KUBEVIRT_UNLIMITEDSWAP=${KUBEVIRT_UNLIMITEDSWAP:-false}
+KUBEVIRT_CPU_MANAGER_POLICY=${KUBEVIRT_CPU_MANAGER_POLICY:-none}
 
 # If on a developer setup, expose ocp on 8443, so that the openshift web console can be used (the port is important because of auth redirects)
 # http and https ports are accessed by testing framework and should not be randomized
