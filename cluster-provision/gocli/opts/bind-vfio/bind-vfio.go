96ba5a0fca9d0e1eeeed5c7c2d9d5cf15c76fdc3
aerosouund <aerosound161@gmail.com>
2024-06-26 12:54:59 +0300

    load vfio kernel module with retries in vfio opt
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cluster-provision/gocli/opts/bind-vfio/bind-vfio.go
+++ cluster-provision/gocli/opts/bind-vfio/bind-vfio.go
@@ -3,26 +3,29 @@ package bindvfio
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	utils "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/ssh"
 )
 
 type BindVfioOpt struct {
-	sshPort uint16
-	nodeIdx int
-	pciID   string
+	sshPort   uint16
+	nodeIdx   int
+	pciID     string
+	sshClient utils.SSHClient
 }
 
-func NewBindVfioOpt(sshPort uint16, nodeIdx int, id string) *BindVfioOpt {
+func NewBindVfioOpt(sshClient utils.SSHClient, sshPort uint16, nodeIdx int, id string) *BindVfioOpt {
 	return &BindVfioOpt{
-		sshPort: sshPort,
-		nodeIdx: nodeIdx,
-		pciID:   id,
+		sshPort:   sshPort,
+		nodeIdx:   nodeIdx,
+		pciID:     id,
+		sshClient: sshClient,
 	}
 }
 
 func (o *BindVfioOpt) Exec() error {
-	addr, err := utils.JumpSSH(o.sshPort, o.nodeIdx, `lspci -D -d `+o.pciID, true, true)
+	addr, err := o.sshClient.JumpSSH(o.sshPort, o.nodeIdx, "lspci -D -d "+o.pciID, true, false)
 	if err != nil {
 		return err
 	}
@@ -33,30 +36,37 @@ func (o *BindVfioOpt) Exec() error {
 	driverPath := devSysfsPath + "/driver"
 	driverOverride := devSysfsPath + "/driver_override"
 
-	driver, err := utils.JumpSSH(o.sshPort, o.nodeIdx, "readlink "+driverPath+" | awk -F'/' '{print $NF}'", true, true)
+	driver, err := o.sshClient.JumpSSH(o.sshPort, o.nodeIdx, "readlink "+driverPath+" | awk -F'/' '{print $NF}'", true, false)
 	if err != nil {
 		return err
+	}
+	driver = strings.TrimSuffix(driver, "\n")
+
+	if _, err := o.sshClient.JumpSSH(o.sshPort, o.nodeIdx, "modprobe -i vfio-pci", true, false); err != nil {
+		return fmt.Errorf("Error loading vfio-pci module: %v", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		if _, err := o.sshClient.JumpSSH(o.sshPort, o.nodeIdx, "ls /sys/bus/pci/drivers/vfio-pci", true, false); err != nil {
+			fmt.Println("module not loaded properly, sleeping 1 second and trying again")
+			time.Sleep(time.Second * 1)
+			o.sshClient.JumpSSH(o.sshPort, o.nodeIdx, "modprobe -i vfio-pci", true, false)
+		} else {
+			break
+		}
 	}
 
 	cmds := []string{
 		"if [[ ! -d /sys/bus/pci/devices/" + pciDevId + " ]]; then echo 'Error: PCI address does not exist!' && exit 1; fi",
 		"if [[ ! -d /sys/bus/pci/devices/" + pciDevId + "/iommu/ ]]; then echo 'Error: No vIOMMU found in the VM' && exit 1; fi",
-		"modprobe -i vfio-pci",
-		"[[ " + driver + "!= 'vfio-pci' ]] && echo " + pciDevId + " > " + driverPath + "/unbind && echo 'vfio-pci' > " + driverOverride + " && echo " + pciDevId + " > /sys/bus/pci/drivers/vfio-pci/bind",
+		"[[ '" + driver + "' != 'vfio-pci' ]] && echo " + pciDevId + " > " + driverPath + "/unbind && echo 'vfio-pci' > " + driverOverride + " && echo " + pciDevId + " > /sys/bus/pci/drivers/vfio-pci/bind",
 	}
 
 	for _, cmd := range cmds {
-		if _, err := utils.JumpSSH(o.sshPort, 1, cmd, true, true); err != nil {
+		if _, err := o.sshClient.JumpSSH(o.sshPort, o.nodeIdx, cmd, true, true); err != nil {
 			return err
 		}
 	}
 
-	newDriver, err := utils.JumpSSH(o.sshPort, o.nodeIdx, "readlink "+driverPath+" | awk -F'/' '{print $NF}'", true, true)
-	if err != nil {
-		return err
-	}
-	if newDriver != "vfio-pci" {
-		return fmt.Errorf("Error: Failed to bind to vfio-pci driver")
-	}
 	return nil
 }
