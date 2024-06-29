e7e97797543c6541df4cf1572b7b5200cba1a349
aerosouund <aerosound161@gmail.com>
2024-06-29 16:06:58 +0300

    feat: Create DockerAdapter type
    The DockerAdapter type changes the previously used docker.Exec ssh.sh method and conforms it into the SSHClient interface. By taking the command and modifying it to be runnable with ssh.sh based on whether its a script, script with params or direct binary command.
    The node container field indicates the name of the container that ran the qemu command that corresponds to the target ssh node
    
    Signed-off-by: aerosouund <aerosound161@gmail.com>

---
+++ cluster-provision/gocli/docker/adapter.go
@@ -0,0 +1,48 @@
+package docker
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/docker/docker/client"
+)
+
+// DockerAdapter is a wrapper around client.Client to conform it to the SSH interface
+type DockerAdapter struct {
+	nodeName     string
+	dockerClient *client.Client
+}
+
+func NewAdapter(cli *client.Client, nodeName string) *DockerAdapter {
+	return &DockerAdapter{
+		nodeName:     nodeName,
+		dockerClient: cli,
+	}
+}
+
+func (d *DockerAdapter) Command(cmd string) error {
+	if len(cmd) > 0 {
+		firstCmdChar := cmd[0]
+		switch string(firstCmdChar) {
+		// directly runnable script
+		case "/":
+			cmd = "ssh.sh sudo /bin/bash < " + cmd
+		// script with parameters
+		case "-":
+			cmd = "ssh.sh sudo /bin/bash " + cmd
+		// ordinary command
+		default:
+			cmd = "ssh.sh " + cmd
+		}
+	}
+
+	success, err := Exec(d.dockerClient, d.nodeName, []string{"/bin/bash", "-c", cmd}, os.Stdout)
+	if err != nil {
+		return err
+	}
+
+	if !success {
+		return fmt.Errorf("Error executing %s on node %s", cmd, d.nodeName)
+	}
+	return nil
+}
