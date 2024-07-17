c53160bb5ea1d2ac264c7242e77bc2712728452f
aerosouund <aerosound161@gmail.com>
2024-07-17 12:53:29 +0300

    refactor: Introduce the previously pure bash options to nodeconfig package
    Some options for the kubevirt cluster were solely dependent on the bash code, this adds them to the go code
    As well as refactor the constructors for configs to the the options pattern to avoid having a constructor that takes too many args
    
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cluster-provision/gocli/cmd/nodesconfig/nodeconfig.go
+++ cluster-provision/gocli/cmd/nodesconfig/nodeconfig.go
@@ -1,21 +1,28 @@
 package nodesconfig
 
-// NodeLinuxConfig type holds the config params that a node can have for its linux system
+// NodeLinuxConfig type is a holder for all the config params that a node can have for its linux system
 type NodeLinuxConfig struct {
-	NodeIdx      int
-	K8sVersion   string
-	FipsEnabled  bool
-	DockerProxy  string
-	EtcdInMemory bool
-	EtcdSize     string
-	SingleStack  bool
-	EnableAudit  bool
-	GpuAddress   string
-	Realtime     bool
-	PSA          bool
+	NodeIdx         int
+	K8sVersion      string
+	FipsEnabled     bool
+	DockerProxy     string
+	EtcdInMemory    bool
+	EtcdSize        string
+	SingleStack     bool
+	EnableAudit     bool
+	GpuAddress      string
+	Realtime        bool
+	PSA             bool
+	KsmEnabled      bool
+	SwapEnabled     bool
+	KsmPageCount    int
+	KsmScanInterval int
+	Swapiness       int
+	UnlimitedSwap   bool
+	SwapSize        int
 }
 
-// NodeK8sConfig type holds the config k8s options for kubevirt cluster
+// NodeK8sConfig type is a holder for all the config k8s options for kubevirt cluster
 type NodeK8sConfig struct {
 	Ceph         bool
 	Prometheus   bool
@@ -23,32 +30,33 @@ type NodeK8sConfig struct {
 	Grafana      bool
 	Istio        bool
 	NfsCsi       bool
+	Cnao         bool
+	Multus       bool
+	Cdi          bool
+	CdiVersion   string
+	AAQ          bool
+	AAQVersion   string
 }
 
-func NewNodeK8sConfig(ceph, prometheus, alertmanager, grafana, istio, nfsCsi bool) *NodeK8sConfig {
-	return &NodeK8sConfig{
-		Ceph:         ceph,
-		Prometheus:   prometheus,
-		Alertmanager: alertmanager,
-		Grafana:      grafana,
-		Istio:        istio,
-		NfsCsi:       nfsCsi,
+func NewNodeK8sConfig(confs []K8sConfigFunc) *NodeK8sConfig {
+	n := &NodeK8sConfig{}
+
+	for _, conf := range confs {
+		conf(n)
 	}
+
+	return n
 }
 
-func NewNodeLinuxConfig(nodeIdx int, k8sVersion, dockerProxy, etcdSize, gpuAddress string,
-	fipsEnabled, etcdInMemory, singleStack, enableAudit, realtime, psa bool) *NodeLinuxConfig {
-	return &NodeLinuxConfig{
-		NodeIdx:      nodeIdx,
-		K8sVersion:   k8sVersion,
-		FipsEnabled:  fipsEnabled,
-		DockerProxy:  dockerProxy,
-		EtcdInMemory: etcdInMemory,
-		EtcdSize:     etcdSize,
-		SingleStack:  singleStack,
-		EnableAudit:  enableAudit,
-		GpuAddress:   gpuAddress,
-		Realtime:     realtime,
-		PSA:          psa,
+func NewNodeLinuxConfig(nodeIdx int, k8sVersion string, confs []LinuxConfigFunc) *NodeLinuxConfig {
+	n := &NodeLinuxConfig{
+		NodeIdx:    nodeIdx,
+		K8sVersion: k8sVersion,
 	}
+
+	for _, conf := range confs {
+		conf(n)
+	}
+
+	return n
 }
