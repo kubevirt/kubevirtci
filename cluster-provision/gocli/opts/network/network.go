44215d25e232fb47655084e1853ada92bfda5719
aerosouund <aerosound161@gmail.com>
2024-07-28 13:17:00 +0300

    feat: Introduce opts for multiple system tasks
    Remounting sysfs as readwrite.
    K8s networking configuration.
    Downloading and executing registry proxy scripts.
    
    Signed-off-by: aerosouund <aerosound161@gmail.com>

---
+++ cluster-provision/gocli/opts/network/network.go
@@ -0,0 +1,29 @@
+package network
+
+import "kubevirt.io/kubevirtci/cluster-provision/gocli/pkg/libssh"
+
+type NetworkOpt struct {
+	sshClient libssh.Client
+}
+
+func NewNetworkOpt(sshClient libssh.Client) *NetworkOpt {
+	return &NetworkOpt{
+		sshClient: sshClient,
+	}
+}
+
+func (n *NetworkOpt) Exec() error {
+	cmds := []string{
+		"modprobe br_netfilter",
+		"sysctl -w net.bridge.bridge-nf-call-arptables=1",
+		"sysctl -w net.bridge.bridge-nf-call-iptables=1",
+		"sysctl -w net.bridge.bridge-nf-call-ip6tables=1",
+	}
+
+	for _, cmd := range cmds {
+		if err := n.sshClient.Command(cmd); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ cluster-provision/gocli/opts/registryproxy/registryproxy.go
@@ -0,0 +1,32 @@
+package registryproxy
+
+import "kubevirt.io/kubevirtci/cluster-provision/gocli/pkg/libssh"
+
+type RegistryProxyOpt struct {
+	sshClient libssh.Client
+	proxyUrl  string
+}
+
+func NewRegistryProxyOpt(sshClient libssh.Client, proxyUrl string) *RegistryProxyOpt {
+	return &RegistryProxyOpt{
+		sshClient: sshClient,
+		proxyUrl:  proxyUrl,
+	}
+}
+
+func (rp *RegistryProxyOpt) Exec() error {
+	setupUrl := "http://" + rp.proxyUrl + ":3128/setup/systemd"
+	cmds := []string{
+		"curl " + setupUrl + " > proxyscript.sh",
+		"sed s/docker.service/containerd.service/g proxyscript.sh",
+		`sed '/Environment/ s/$/ \"NO_PROXY=127.0.0.0\/8,10.0.0.0\/8,172.16.0.0\/12,192.168.0.0\/16\"/ proxyscript.sh`,
+		"source proxyscript.sh",
+	}
+	for _, cmd := range cmds {
+		if err := rp.sshClient.Command(cmd); err != nil {
+			return err
+		}
+	}
+	return nil
+
+}
