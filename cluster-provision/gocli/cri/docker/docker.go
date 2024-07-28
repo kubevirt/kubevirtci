502d96a386d6c3257c5b0575a61918949f1b7ed7
aerosouund <aerosound161@gmail.com>
2024-07-28 13:00:20 +0300

    feat: Introduce the CRI package primitive interfaces
    The ContainerClient is an abstraction to facilitate using a container runtime on the host regardless of what it is.
    The CreateOpts type is a holder of the container run options required in kubevirt ci.
    
    Signed-off-by: aerosouund <aerosound161@gmail.com>

---
+++ cluster-provision/gocli/cri/docker/docker.go
@@ -0,0 +1,100 @@
+package docker
+
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"kubevirt.io/kubevirtci/cluster-provision/gocli/cri"
+)
+
+type DockerClient struct{}
+
+func NewDockerClient() *DockerClient {
+	return &DockerClient{}
+}
+
+func IsAvailable() bool {
+	cmd := exec.Command("docker", "-v")
+	out, err := cmd.Output()
+	if err != nil {
+		return false
+	}
+	return strings.HasPrefix(string(out), "Docker version")
+}
+
+func (dc *DockerClient) ImagePull(image string) error {
+	cmd := exec.Command("docker", "pull", image)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (dc *DockerClient) Inspect(containerID, format string) ([]byte, error) {
+	cmd := exec.Command("docker", "inspect", containerID, "--format", format)
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (dc *DockerClient) Start(containerID string) error {
+	cmd := exec.Command("docker",
+		"start",
+		containerID)
+
+	if _, err := cmd.CombinedOutput(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (dc *DockerClient) Create(image string, createOpts *cri.CreateOpts) (string, error) {
+	ports := ""
+	for containerPort, hostPort := range createOpts.Ports {
+		ports += "-p " + containerPort + ":" + hostPort
+	}
+
+	args := []string{
+		"--name=" + createOpts.Name,
+		"--privileged=" + strconv.FormatBool(createOpts.Privileged),
+		"--rm=" + strconv.FormatBool(createOpts.Remove),
+		"--restart=" + createOpts.RestartPolicy,
+		"--network=" + createOpts.Network,
+	}
+
+	for containerPort, hostPort := range createOpts.Ports {
+		args = append(args, "-p", containerPort+":"+hostPort)
+	}
+
+	if len(createOpts.Capabilities) > 0 {
+		args = append(args, "--cap-add="+strings.Join(createOpts.Capabilities, ","))
+	}
+
+	fullArgs := append([]string{"create"}, args...)
+	fullArgs = append(fullArgs, image)
+	fullArgs = append(fullArgs, createOpts.Command...)
+
+	cmd := exec.Command("docker",
+		fullArgs...,
+	)
+
+	containerID, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	logrus.Info("created registry container with id: ", string(containerID))
+	return strings.TrimSuffix(string(containerID), "\n"), nil
+}
+
+func (dc *DockerClient) Remove(containerID string) error {
+	cmd := exec.Command("docker", "rm", "-f", containerID)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
