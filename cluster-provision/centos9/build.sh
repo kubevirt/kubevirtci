b6d2495d279731a944b1cc1c5e1f8776efdaa1b7
chandramerla <Chandra.Merla@ibm.com>
2024-08-08 10:15:19 +0530

    feat: Updated Centos9 Dockerfile and its CMD script to support s390x arch
    - Added conditional execution in Dockerfile based on arch
    - Updated Dockerfile CMD script (vm.sh) with qemu-system-s390x and its args as per what are supported on s390x
    
    Signed-off-by: chandramerla <Chandra.Merla@ibm.com>

--- cluster-provision/centos9/build.sh
+++ cluster-provision/centos9/build.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 centos_version="$(cat $DIR/version | tr -d '\n')"
 
-docker build --build-arg centos_version=$centos_version . -t quay.io/kubevirtci/centos9
+docker build --build-arg BUILDARCH=$(uname -m) --build-arg centos_version=$centos_version . -t quay.io/kubevirtci/centos9
