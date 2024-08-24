3ccedec18616ebc30c4fca921a8f96d970ae6767
aerosouund <aerosound161@gmail.com>
2024-08-24 15:03:19 +0300

    refactor!: Modify cluster up to use the gocli
    Delete old bash code and up clusters using the gocli instead.
    
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cluster-up/cluster/kind-1.28/provider.sh
+++ cluster-up/cluster/kind-1.28/provider.sh
@@ -2,52 +2,16 @@
 
 set -e
 
-DEFAULT_CLUSTER_NAME="kind-1.28"
-DEFAULT_HOST_PORT=5000
-ALTERNATE_HOST_PORT=5001
-export CLUSTER_NAME=${CLUSTER_NAME:-$DEFAULT_CLUSTER_NAME}
+export CLUSTER_NAME="k8s-1.28"
 
-if [ $CLUSTER_NAME == $DEFAULT_CLUSTER_NAME ]; then
-    export HOST_PORT=$DEFAULT_HOST_PORT
-else
-    export HOST_PORT=$ALTERNATE_HOST_PORT
-fi
-
-function set_kind_params() {
-    version=$(cat cluster-up/cluster/$KUBEVIRT_PROVIDER/version)
-    export KIND_VERSION="${KIND_VERSION:-$version}"
-
-    image=$(cat cluster-up/cluster/$KUBEVIRT_PROVIDER/image)
-    export KIND_NODE_IMAGE="${KIND_NODE_IMAGE:-$image}"
-}
-
-function configure_registry_proxy() {
-    [ "$CI" != "true" ] && return
-
-    echo "Configuring cluster nodes to work with CI mirror-proxy..."
-
-    local -r ci_proxy_hostname="docker-mirror-proxy.kubevirt-prow.svc"
-    local -r kind_binary_path="${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/.kind"
-    local -r configure_registry_proxy_script="${KUBEVIRTCI_PATH}/cluster/kind/configure-registry-proxy.sh"
-
-    KIND_BIN="$kind_binary_path" PROXY_HOSTNAME="$ci_proxy_hostname" $configure_registry_proxy_script
-}
+make -C cluster-provision/gocli cli
 
 function up() {
-    cp $KIND_MANIFESTS_DIR/kind.yaml ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/kind.yaml
-    _add_kubeadm_cpu_manager_config_patch
-    _add_extra_mounts
-    export CONFIG_WORKER_CPU_MANAGER=true
-    kind_up
-
-    configure_registry_proxy
-
-    # remove the rancher.io kind default storageClass
-    _kubectl delete sc standard
-
-    echo "$KUBEVIRT_PROVIDER cluster '$CLUSTER_NAME' is ready"
+    if [ "$CI" == "true" ]; then export REGISTRY_PROXY="docker-mirror-proxy.kubevirt-prow.svc"; fi
+    ./cluster-provision/gocli/build/cli run-kind k8s-1.28 \
+        --with-extra-mounts=true \
+        --nodes=$KUBEVIRT_NUM_NODES \
+        --registry-proxy=$REGISTRY_PROXY
 }
-
-set_kind_params
 
 source ${KUBEVIRTCI_PATH}/cluster/kind/common.sh
