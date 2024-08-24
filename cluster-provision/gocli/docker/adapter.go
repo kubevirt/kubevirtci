a30df55405c88204b2c3bc80b1c8f7362a3af956
aerosouund <aerosound161@gmail.com>
2024-08-24 14:44:57 +0300

    refactor: Generalize the Docker adapter to execute general commands and extend it
    The docker adapter was previously used to execute commands when ssh.sh was still used.
    Its removed and the adapter is changed to be a general interface for container interaction.
    Implement the full ssh client interface on the adapter.
    
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cluster-provision/gocli/docker/adapter.go
+++ cluster-provision/gocli/docker/adapter.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"os"
 
+	"bytes"
+	"context"
+	"io"
+
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
@@ -21,28 +26,48 @@ func NewAdapter(cli *client.Client, nodeName string) *DockerAdapter {
 }
 
 func (d *DockerAdapter) Command(cmd string) error {
-	if len(cmd) > 0 {
-		firstCmdChar := cmd[0]
-		switch string(firstCmdChar) {
-		// directly runnable script
-		case "/":
-			cmd = "ssh.sh sudo /bin/bash < " + cmd
-		// script with parameters
-		case "-":
-			cmd = "ssh.sh sudo /bin/bash " + cmd
-		// ordinary command
-		default:
-			cmd = "ssh.sh " + cmd
-		}
-	}
-
-	success, err := Exec(d.dockerClient, d.nodeName, []string{"/bin/bash", "-c", cmd}, os.Stdout)
+	success, err := Exec(d.dockerClient, d.nodeName, []string{"/bin/sh", "-c", cmd}, os.Stdout)
 	if err != nil {
 		return err
 	}
 
 	if !success {
 		return fmt.Errorf("Error executing %s on node %s", cmd, d.nodeName)
+	}
+	return nil
+}
+
+func (d *DockerAdapter) CommandWithNoStdOut(cmd string) (string, error) {
+	var buf *bytes.Buffer
+	success, err := Exec(d.dockerClient, d.nodeName, []string{"/bin/sh", "-c", cmd}, buf)
+	if err != nil {
+		return "", err
+	}
+
+	if !success {
+		return "", fmt.Errorf("Error executing %s on node %s", cmd, d.nodeName)
+	}
+	return buf.String(), nil
+}
+
+func (d *DockerAdapter) SCP(destPath string, contents io.Reader) error {
+	return d.dockerClient.CopyToContainer(context.Background(), d.nodeName, destPath, contents, types.CopyToContainerOptions{})
+}
+
+func (d *DockerAdapter) CopyRemoteFile(remotePath string, out io.Writer) error {
+	defer os.Remove("tempfile")
+	if _, _, err := d.dockerClient.CopyFromContainer(context.Background(), d.nodeName, "tempfile"); err != nil {
+		return err
+	}
+
+	tempfile, err := os.ReadFile("tempfile")
+	if err != nil {
+		return err
+	}
+
+	_, err = out.Write(tempfile)
+	if err != nil {
+		return err
 	}
 	return nil
 }
