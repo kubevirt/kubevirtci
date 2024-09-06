e0c0250e2754d6f2a99499023c5e954a57b05d27
aerosouund <aerosound161@gmail.com>
2024-09-06 12:19:49 +0300

    test: Create test cases to assert CNAO is creating the addons config with and without Multus properly when passing the multusEnabled argument
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cluster-provision/gocli/opts/cnao/cnao_test.go
+++ cluster-provision/gocli/opts/cnao/cnao_test.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"go.uber.org/mock/gomock"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	k8s "kubevirt.io/kubevirtci/cluster-provision/gocli/pkg/k8s"
 	kubevirtcimocks "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/mock"
 )
@@ -27,16 +28,41 @@ var _ = Describe("CnaoOpt", func() {
 		mockCtrl = gomock.NewController(GinkgoT())
 		client = k8s.NewTestClient()
 		sshClient = kubevirtcimocks.NewMockSSHClient(mockCtrl)
-		opt = NewCnaoOpt(client, sshClient, false)
 	})
 
 	AfterEach(func() {
 		mockCtrl.Finish()
 	})
 
-	It("should execute CnaoOpt successfully", func() {
+	It("should execute create CNAO with Multus", func() {
+		opt = NewCnaoOpt(client, sshClient, false)
 		sshClient.EXPECT().Command("kubectl --kubeconfig=/etc/kubernetes/admin.conf wait deployment -n cluster-network-addons cluster-network-addons-operator --for condition=Available --timeout=200s")
-		err := opt.Exec()
+		opt.Exec()
+
+		obj, err := client.Get(schema.GroupVersionKind{Group: "networkaddonsoperator.network.kubevirt.io",
+			Version: "v1",
+			Kind:    "NetworkAddonsConfig"}, "cluster", "")
 		Expect(err).NotTo(HaveOccurred())
+
+		spec, ok := obj.Object["spec"].(map[string]interface{})
+		Expect(ok).To(Equal(true))
+		Expect(spec).To(HaveKey("multus"))
+		Expect(spec).To(HaveKey("multusDynamicNetworks"))
+	})
+
+	It("should execute create CNAO without Multus", func() {
+		opt = NewCnaoOpt(client, sshClient, true)
+		sshClient.EXPECT().Command("kubectl --kubeconfig=/etc/kubernetes/admin.conf wait deployment -n cluster-network-addons cluster-network-addons-operator --for condition=Available --timeout=200s")
+		opt.Exec()
+
+		obj, err := client.Get(schema.GroupVersionKind{Group: "networkaddonsoperator.network.kubevirt.io",
+			Version: "v1",
+			Kind:    "NetworkAddonsConfig"}, "cluster", "")
+		Expect(err).NotTo(HaveOccurred())
+
+		spec, ok := obj.Object["spec"].(map[string]interface{})
+		Expect(ok).To(Equal(true))
+		Expect(spec).NotTo(HaveKey("multus"))
+		Expect(spec).NotTo(HaveKey("multusDynamicNetworks"))
 	})
 })
