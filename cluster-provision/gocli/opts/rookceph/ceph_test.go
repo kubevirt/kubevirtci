eac333ea37f8b3734d6a166d97372b746f4cf01e
aerosouund <aerosound161@gmail.com>
2024-09-09 12:45:06 +0300

    test: Add mock ssh client to rook ceph testing and add expect calls for the commands
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cluster-provision/gocli/opts/rookceph/ceph_test.go
+++ cluster-provision/gocli/opts/rookceph/ceph_test.go
@@ -5,7 +5,9 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
 	k8s "kubevirt.io/kubevirtci/cluster-provision/gocli/pkg/k8s"
+	kubevirtcimocks "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/mock"
 )
 
 func TestNfsCsiOpt(t *testing.T) {
@@ -17,15 +19,18 @@ var _ = Describe("CephOpt", func() {
 	var (
 		k8sClient k8s.K8sDynamicClient
 		opt       *cephOpt
+		sshClient *kubevirtcimocks.MockSSHClient
 	)
 
 	BeforeEach(func() {
+		sshClient = kubevirtcimocks.NewMockSSHClient(gomock.NewController(GinkgoT()))
+		AddExpectCalls(sshClient)
 		r := k8s.NewReactorConfig("create", "cephblockpools", CephReactor)
 		k8sClient = k8s.NewTestClient(r)
-		opt = NewCephOpt(k8sClient)
+		opt = NewCephOpt(k8sClient, sshClient)
 	})
 
-	It("should execute NfsCsiOpt successfully", func() {
+	It("should execute Ceph successfully", func() {
 		err := opt.Exec()
 		Expect(err).NotTo(HaveOccurred())
 	})
--- cluster-provision/gocli/opts/rookceph/testconfig.go
+++ cluster-provision/gocli/opts/rookceph/testconfig.go
@@ -4,6 +4,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	k8stesting "k8s.io/client-go/testing"
+	kubevirtcimocks "kubevirt.io/kubevirtci/cluster-provision/gocli/utils/mock"
 )
 
 var CephReactor = func(action k8stesting.Action) (bool, runtime.Object, error) {
@@ -16,4 +17,9 @@ var CephReactor = func(action k8stesting.Action) (bool, runtime.Object, error) {
 		return true, nil, err
 	}
 	return false, obj, nil
+}
+
+func AddExpectCalls(sshClient *kubevirtcimocks.MockSSHClient) {
+	sshClient.EXPECT().Command(`kubectl --kubeconfig /etc/kubernetes/admin.conf patch storageclass local -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'`)
+	sshClient.EXPECT().Command(`kubectl --kubeconfig /etc/kubernetes/admin.conf patch storageclass rook-ceph-block -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'`)
 }
