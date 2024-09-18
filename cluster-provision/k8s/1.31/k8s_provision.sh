459481d9e6fe0a612eba0d2e7b05fb08ab91ecb6
Brian Carey <bcarey@redhat.com>
2024-09-18 16:16:16 +0100

    k8s-1.31: Update cri-o to version v1.31.0
    cri-o version v1.31.0 has been released[1] and is available in the
    kubevirt mirror[2]
    
    [1] https://github.com/cri-o/cri-o/releases/tag/v1.31.0
    [2] https://prow.ci.kubevirt.io/view/gs/kubevirt-prow/logs/periodic-kubevirtci-mirror-crio-repository-weekly/1835475127676964864
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>

--- cluster-provision/k8s/1.31/k8s_provision.sh
+++ cluster-provision/k8s/1.31/k8s_provision.sh
@@ -62,7 +62,7 @@ function pull_container_retry() {
     fi
 }
 
-export CRIO_VERSION=1.30
+export CRIO_VERSION=1.31
 cat << EOF >/etc/yum.repos.d/devel_kubic_libcontainers_stable_cri-o_${CRIO_VERSION}.repo
 [isv_kubernetes_addons_cri-o_stable_v${CRIO_VERSION}]
 name=CRI-O v${CRIO_VERSION} (Stable) (rpm)
