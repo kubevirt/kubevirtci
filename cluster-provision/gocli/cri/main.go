c68122c096efe2fc2b8f4d973b61467b86d990e8
aerosouund <aerosound161@gmail.com>
2024-09-30 18:47:25 +0300

    feat: CRI package
    Signed-off-by: aerosouund <aerosound161@gmail.com>

---
+++ cluster-provision/gocli/cri/main.go
@@ -0,0 +1,23 @@
+package cri
+
+// maybe just create wrappers around bash after all
+type ContainerClient interface {
+	ImagePull(image string) error
+	Create(image string, co *CreateOpts) (string, error)
+	Start(containerID string) error
+	Remove(containerID string) error
+	Inspect(containerID, format string) ([]byte, error)
+	Build(tag, containerFile string, buildArgs map[string]string) error
+	Run(runArgs []string) error
+}
+
+type CreateOpts struct {
+	Privileged    bool
+	Name          string
+	Ports         map[string]string
+	RestartPolicy string
+	Network       string
+	Command       []string
+	Remove        bool
+	Capabilities  []string
+}
