84283622ea8c15987821463e41f2ec5ab18724e5
Andrea Bolognani <abologna@redhat.com>
2024-10-11 11:33:01 +0200

    k8s: Make it work even if kubectl is not installed on the machine
    There's code in the k8s provider that takes care of pulling the
    kubectl binary from the cluster and use that one for subsequent
    operations; however, right before that happens, a few settings
    are changed in kubeconfig, which means that it's necessary for
    the host system to provide its own kubectl binary.
    
    Invert the order of these operations, and make sure that we
    always invoke the binary obtained from the cluster. This way, it
    is possible to successfully bring up a cluster on a machine that
    doesn't have kubectl installed.
    
    https://github.com/kubevirt/kubevirtci/issues/1293
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- cluster-up/cluster/k8s-provider-common.sh
+++ cluster-up/cluster/k8s-provider-common.sh
@@ -48,15 +48,14 @@ function up() {
     eval ${_cli:?} run $params
 
     ${_cli} scp --prefix $provider_prefix /etc/kubernetes/admin.conf - >${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/.kubeconfig
-
-    # Set server and disable tls check
-    export KUBECONFIG=${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/.kubeconfig
-    kubectl config set-cluster kubernetes --server="https://$(_main_ip):$(_port k8s)"
-    kubectl config set-cluster kubernetes --insecure-skip-tls-verify=true
-
     ${_cli} scp --prefix ${provider_prefix:?} /usr/bin/kubectl - >${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/.kubectl
     
     chmod u+x ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/.kubectl
+
+    # Set server and disable tls check
+    export KUBECONFIG=${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/.kubeconfig
+    ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/.kubectl config set-cluster kubernetes --server="https://$(_main_ip):$(_port k8s)"
+    ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/.kubectl config set-cluster kubernetes --insecure-skip-tls-verify=true
 
     # Make sure that local config is correct
     prepare_config
