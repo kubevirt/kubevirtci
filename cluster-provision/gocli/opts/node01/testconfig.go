7f9de3e15bfa910a181770075505e70cd136b493
Brian Carey <bcarey@redhat.com>
2024-10-23 08:42:57 +0100

    nodes: Fix wait for cri-o to not exit 1 when cri-o is ready
    Currently if the cri-o service is not ready, and the wait is triggered -
    once cri-o becomes ready the wait will exit 1 which causes the prowjobs
    to fail[1]
    
    This update will only exit 1 if the timeout is reached.
    
    [1] https://prow.ci.kubevirt.io/view/gs/kubevirt-prow/pr-logs/pull/kubevirt_kubevirt/13096/pull-kubevirt-e2e-k8s-1.31-sig-compute/1848977885352693760#1:build-log.txt%3A313
    
    Signed-off-by: Brian Carey <bcarey@redhat.com>

--- cluster-provision/gocli/opts/node01/testconfig.go
+++ cluster-provision/gocli/opts/node01/testconfig.go
@@ -8,7 +8,7 @@ func AddExpectCalls(sshClient *kubevirtcimocks.MockSSHClient) {
 		`timeout=30; interval=5; while ! hostnamectl | grep Transient; do echo "Waiting for dhclient to set the hostname from dnsmasq"; sleep $interval; timeout=$((timeout - interval)); [ $timeout -le 0 ] && exit 1; done`,
 		"swapoff -a",
 		"until ip address show dev eth0 | grep global | grep inet6; do sleep 1; done",
-		`timeout=60; interval=5; while ! systemctl status crio | grep active; do echo "Waiting for cri-o service to be ready"; sleep $interval; timeout=$((timeout - interval)); [ $timeout -le 0 ] && exit 1; done`,
+		`timeout=60; interval=5; while ! systemctl status crio | grep -w "active"; do echo "Waiting for cri-o service to be ready"; sleep $interval; timeout=$((timeout - interval)); if [[ $timeout -le 0 ]]; then exit 1; fi; done`,
 		`kubeadm init --config /etc/kubernetes/kubeadm.conf -v5`,
 		`kubectl --kubeconfig=/etc/kubernetes/admin.conf patch deployment coredns -n kube-system -p "$(cat /provision/kubeadm-patches/add-security-context-deployment-patch.yaml)"`,
 		`kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f /provision/cni.yaml`,
