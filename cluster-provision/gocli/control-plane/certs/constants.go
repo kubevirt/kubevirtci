9ad1c14f89e884efde7fca6d209bf5737ad775c7
aerosouund <aerosound161@gmail.com>
2024-11-05 11:49:47 +0200

    use 110 as the container control plane ip
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cluster-provision/gocli/control-plane/certs/constants.go
+++ cluster-provision/gocli/control-plane/certs/constants.go
@@ -6,4 +6,4 @@ var ApiServerDnsNames = []string{"kubernetes", "kubernetes.default",
 	"kubernetes.default.svc", "kubernetes.default.svc.cluster",
 	"kubernetes.svc.cluster.local", "api-server.kubernetes.local"}
 
-var ApiServerIPs = []net.IP{net.ParseIP("127.0.0.1"), net.ParseIP("10.96.0.1"), net.ParseIP("192.168.66.101")}
+var ApiServerIPs = []net.IP{net.ParseIP("127.0.0.1"), net.ParseIP("10.96.0.1"), net.ParseIP("192.168.66.110")}
