7484fbe0e414b4b590fb40598bf3aa97c4694c39
aerosouund <aerosound161@gmail.com>
2024-12-15 20:59:55 +0200

    refactor: Patch the CNI security context in another thread since higher privileges are needed
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cluster-provision/gocli/opts/istio/istio.go
+++ cluster-provision/gocli/opts/istio/istio.go
@@ -2,12 +2,12 @@ package istio
 
 import (
 	_ "embed"
-	"fmt"
 	"time"
 
 	"github.com/cenkalti/backoff/v4"
 	"github.com/sirupsen/logrus"
-	istiov1alpha1 "istio.io/operator/pkg/apis/istio/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	k8s "kubevirt.io/kubevirtci/cluster-provision/gocli/pkg/k8s"
@@ -49,9 +49,15 @@ func (o *istioOpt) Exec() error {
 		return err
 	}
 
+	istioFile := "/opt/istio-operator-with-cnao.yaml"
+	if !o.cnaoEnabled {
+		istioFile = "/opt/istio-operator.cr.yaml"
+	}
+
 	cmds := []string{
 		"source /var/lib/kubevirtci/shared_vars.sh",
-		"PATH=/opt/istio-" + istioVersion + "/bin:$PATH istioctl --kubeconfig /etc/kubernetes/admin.conf install -y",
+		`echo '` + string(istioWithCnao) + `' |  tee /opt/istio-operator-with-cnao.yaml > /dev/null`,
+		`echo '` + string(istioNoCnao) + `' |  tee /opt/istio-operator.cr.yaml > /dev/null`,
 	}
 	for _, cmd := range cmds {
 		if err := o.sshClient.Command(cmd); err != nil {
@@ -59,60 +65,41 @@ func (o *istioOpt) Exec() error {
 		}
 	}
 
-	obj, err = k8s.SerializeIntoObject(istioWithCnao)
-	if err != nil {
+	go func() {
+		operation := func() error {
+			obj, err := o.client.Get(schema.GroupVersionKind{Group: "apps",
+				Version: "v1",
+				Kind:    "DaemonSet"}, "istio-cni-node", "kube-system")
+			cniDaemonSet := &appsv1.DaemonSet{}
+			err = runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, cniDaemonSet)
+			if err != nil {
+				return err
+			}
+
+			var privileged = true
+			cniDaemonSet.Spec.Template.Spec.Containers[0].SecurityContext.Privileged = &privileged
+			newCniDaemonSet, err := runtime.DefaultUnstructuredConverter.ToUnstructured(cniDaemonSet)
+			if err != nil {
+				return err
+			}
+
+			err = o.client.Apply(&unstructured.Unstructured{Object: newCniDaemonSet})
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+
+		backoffStrategy := backoff.NewExponentialBackOff()
+		backoffStrategy.InitialInterval = 10 * time.Second
+		backoffStrategy.MaxElapsedTime = 3 * time.Minute
+
+		err = backoff.Retry(operation, backoffStrategy)
+	}()
+
+	istioInstallCmd := "PATH=/opt/istio-" + istioVersion + "/bin:$PATH istioctl --kubeconfig /etc/kubernetes/admin.conf install -y -f " + istioFile
+	if err := o.sshClient.Command(istioInstallCmd); err != nil {
 		return err
-	}
-
-	if o.cnaoEnabled {
-		if err := o.client.Apply(obj); err != nil {
-			return err
-		}
-	} else {
-		obj, err = k8s.SerializeIntoObject(istioNoCnao)
-		if err != nil {
-			return err
-		}
-
-		if err := o.client.Apply(obj); err != nil {
-			return err
-		}
-	}
-
-	operator := &istiov1alpha1.IstioOperator{}
-
-	operation := func() error {
-		obj, err := o.client.Get(schema.GroupVersionKind{Group: "install.istio.io",
-			Version: "v1alpha1",
-			Kind:    "IstioOperator"}, "istio-operator", "istio-system")
-
-		err = runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, operator)
-		if err != nil {
-			return err
-		}
-
-		if operator.Status == nil {
-			err := fmt.Errorf("Operator status is still not ready")
-			logrus.Info("Istio operator is still not ready, Backing off and retrying")
-			return err
-		}
-
-		if operator.Status.Status != 3 {
-			err := fmt.Errorf("Istio operator failed to move to Healthy status after max retries")
-			logrus.Info("Istio operator is still not ready, Backing off and retrying")
-			return err
-		}
-
-		return nil
-	}
-
-	backoffStrategy := backoff.NewExponentialBackOff()
-	backoffStrategy.InitialInterval = 10 * time.Second
-	backoffStrategy.MaxElapsedTime = 3 * time.Minute
-
-	err = backoff.Retry(operation, backoffStrategy)
-	if err != nil {
-		return fmt.Errorf("Waiting on istio operator to become ready failed after maximum retries: %v", err)
 	}
 
 	logrus.Info("Istio operator is now ready!")
