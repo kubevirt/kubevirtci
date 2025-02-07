7f585965ea7171af6cd8297bbd989a26447b6643
Daniel Hiller <dhiller@redhat.com>
2025-02-07 16:54:21 +0100

    fix,pre-pull-images: move fetch-images.sh to manual
    since the manifests are not inside the cluster deploy directory any more
    they are not being copied into the directory any more, thus the
    fetch-images.sh will not find any images.
    
    Therefore we move the fetch-images.sh into the main provision/k8s folder
    and replace the per version with a pre-pull-images text file.
    
    This file needs to get updated whenever the manifests inside either the
    k8s version folder or the manifests inside the gocli change.
    
    Thus we also create a script update-pre-pull-images.sh and call that
    whenever the cluster is checked at first.
    
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- cluster-provision/k8s/check-pod-images.sh
+++ cluster-provision/k8s/check-pod-images.sh
@@ -17,8 +17,8 @@ fi
 images_not_in_list=$(mktemp)
 images_from_manifests=$(mktemp)
 trap 'rm -f $images_not_in_list $images_from_manifests' EXIT SIGINT SIGTERM
-$DIR/${provision_dir}/fetch-images.sh "$DIR/${provision_dir}" > "${images_from_manifests}"
-$DIR/${provision_dir}/fetch-images.sh "$DIR/../gocli/opts/" >> "${images_from_manifests}"
+$DIR/fetch-images.sh "$DIR/${provision_dir}" > "${images_from_manifests}"
+$DIR/fetch-images.sh "$DIR/../gocli/opts/" >> "${images_from_manifests}"
 for image in $(${ksh} get pods --all-namespaces -o jsonpath="{..image}" | tr -s '[[:space:]]' '\n' | grep -v 'registry.k8s.io' | sort | uniq); do
     set +e
     if ! grep -q "$image" "${pre_pull_image_file}"; then
