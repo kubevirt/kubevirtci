fb29f2421174343fe030b6de27c4c5675406e83d
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2025-07-02 08:13:47 +0300

    Fix an issue with job names w/o k8s version
    If the job name does not contain the k8s version, as in many periodic
    job, it fails on cluster-provision/gocli/opts/nodes/nodes.go::NewNodesProvisioner
    
    This PR uses the KUBEVIRT_PROVIDER environment variable as a fallback.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- cluster-provision/gocli/opts/nodes/nodes_test.go
+++ cluster-provision/gocli/opts/nodes/nodes_test.go
@@ -1,6 +1,7 @@
 package nodes
 
 import (
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -48,4 +49,27 @@ var _ = Describe("nodes", func() {
 		Entry("should add new fg if 1.33", "k8s-1.33", "--feature-gates=NodeSwap=true,DisableCPUQuotaWithExclusiveCPUs=false"),
 		Entry("should add new fg if 1.37", "k8s-1.37", "--feature-gates=NodeSwap=true,DisableCPUQuotaWithExclusiveCPUs=false"),
 	)
+
+	When("job name does not contain version", func() {
+		DescribeTable("calling featureGateFlag",
+			func(k8sVersion, expectedValue string) {
+				kvProviderOrig, kvProviderDefined := os.LookupEnv(kubevirtProviderEnv)
+
+				Expect(os.Setenv(kubevirtProviderEnv, k8sVersion)).To(Succeed())
+				DeferCleanup(func() {
+					if kvProviderDefined {
+						Expect(os.Setenv(kubevirtProviderEnv, kvProviderOrig)).To(Succeed())
+					} else {
+						Expect(os.Unsetenv(kubevirtProviderEnv)).To(Succeed())
+					}
+				})
+
+				np := NewNodesProvisioner("name-with-no-version", nil, false)
+				Expect(np.featureGatesFlag()).To(BeEquivalentTo(expectedValue))
+			},
+			Entry("should not add new fg if 1.32", "k8s-1.32", "--feature-gates=NodeSwap=true"),
+			Entry("should add new fg if 1.33", "k8s-1.33", "--feature-gates=NodeSwap=true,DisableCPUQuotaWithExclusiveCPUs=false"),
+			Entry("should add new fg if 1.37", "k8s-1.37", "--feature-gates=NodeSwap=true,DisableCPUQuotaWithExclusiveCPUs=false"),
+		)
+	})
 })
