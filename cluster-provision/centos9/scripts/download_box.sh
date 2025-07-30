7d4224427cd669c6f7b234397220b2e4b4b68517
Nestor Acuna Blanco <nestor.acuna@ibm.com>
2025-07-30 15:12:08 +0200

    fix: enable centos9 download curl command fail
    The curl command does not fail when the download is unsuccesful,
    which causes the script to fail at a later point.
    
    Signed-off-by: Nestor Acuna Blanco <nestor.acuna@ibm.com>

--- cluster-provision/centos9/scripts/download_box.sh
+++ cluster-provision/centos9/scripts/download_box.sh
@@ -7,9 +7,9 @@ ARCH=$(uname -m)
 
 #For the s390x architecture, instead of vagrant box image, generic cloud (qcow2) image is used directly.
 if [ "$ARCH" == "s390x" ]; then
-    curl -L $1 -o box.qcow2
+    curl -L --fail $1 -o box.qcow2
 else
-    curl -L $1 | tar -zxvf - box.img
+    curl -L --fail $1 | tar -zxvf - box.img
     qemu-img convert -O qcow2 box.img box.qcow2
     rm box.img
 fi
