e740071e62ba6ed1d48cfc921d95d9e85b0b66bd
Brian Carey <brian.carey@protonmail.com>
2026-01-02 21:34:36 +0000

    publish,alpine: Add separate publish script for the alpine-with-test-tooling image
    There is no reason why the publish of the alpine-with-test-tooling image
    has to be included with the publish of the main kubevirtci cluster
    provider images.
    
    Splitting out the publish of the alpine VM image will help to avoid
    alpine publish failures blocking the delivery of the kubevirtci node
    images to kubevirt/kubevirt
    
    This new script can be run from a separate dedicated postsubmit job
    
    Signed-off-by: Brian Carey <brian.carey@protonmail.com>

---
+++ hack/publish-alpine.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+function detect_cri() {
+    if podman ps >/dev/null 2>&1; then echo podman; elif docker ps >/dev/null 2>&1; then echo docker; fi
+}
+
+export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-$(date +"%y%m%d%H%M")-$(git rev-parse --short HEAD)}
+export CRI_BIN=${CRI_BIN:-$(detect_cri)}
+
+TARGET_REPO="quay.io/kubevirtci"
+TARGET_KUBEVIRT_REPO="quay.io/kubevirt"
+
+function build_alpine_container_disk() {
+  echo "INFO: build alpine container disk"
+  (cd cluster-provision/images/vm-image-builder && ./create-containerdisk.sh alpine-cloud-init)
+  ${CRI_BIN} tag alpine-cloud-init:devel ${TARGET_REPO}/alpine-with-test-tooling-container-disk:${KUBEVIRTCI_TAG}
+  ${CRI_BIN} tag alpine-cloud-init:devel ${TARGET_KUBEVIRT_REPO}/alpine-with-test-tooling-container-disk:devel
+}
+
+function push_alpine_container_disk() {
+  echo "INFO: push alpine container disk"
+  TARGET_IMAGE="${TARGET_REPO}/alpine-with-test-tooling-container-disk:${KUBEVIRTCI_TAG}"
+  podman push $TARGET_IMAGE
+  TARGET_KUBEVIRT_IMAGE="${TARGET_KUBEVIRT_REPO}/alpine-with-test-tooling-container-disk:devel"
+  podman push $TARGET_KUBEVIRT_IMAGE
+}
+
+function publish_alpine_container_disk() {
+  build_alpine_container_disk
+  push_alpine_container_disk
+}
+
+publish_alpine_container_disk
