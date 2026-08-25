54fc7ed0b829f0fb95ee4a3167fc91a8b9c2803d
Denis Ollier <dollierp@redhat.com>
2026-08-25 14:40:19 +0200

    fix(registry.k8s.io): directly pull images from Google registry without CDN
    registry.k8s.io CDN switched to Fastly, which is causing CRI-O / Podman
    to fail to pull some container image blobs.
    
    This change configure them to pull K8S images directly from Google
    Artifact Registry without going through the CDN, until the CDN-related
    issue is resolved.
    
    Signed-off-by: Denis Ollier <dollierp@redhat.com>

--- cluster-provision/k8s/1.36/k8s_provision.sh
+++ cluster-provision/k8s/1.36/k8s_provision.sh
@@ -84,8 +84,6 @@ EOF
 
 dnf install -y cri-o
 
-systemctl enable --now crio
-
 cat << EOF > /etc/containers/registries.conf
 [registries.search]
 registries = ['registry.access.redhat.com', 'registry.fedoraproject.org', 'quay.io', 'docker.io']
@@ -96,6 +94,17 @@ registries = ['registry:5000']
 [registries.block]
 registries = []
 EOF
+
+cat << EOF > /etc/containers/registries.conf.d/registry-k8s-io-no-cdn.conf
+# Workaround for https://github.com/kubernetes/registry.k8s.io/issues/333
+[[registry]]
+location = "registry.k8s.io"
+
+[[registry.mirror]]
+location = "us-central1-docker.pkg.dev/k8s-artifacts-prod/images"
+EOF
+
+systemctl enable --now crio
 
 create_sriov_udev_rule
 
